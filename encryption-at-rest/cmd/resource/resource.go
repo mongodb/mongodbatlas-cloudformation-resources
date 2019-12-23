4311686970a732061dbfd64503c50209810344f3
Marin Salinas <marinssalinas@gmail.com>
2019-12-23 16:22:25 -0600

    feat: implement create handler function

--- encryption-at-rest/cmd/resource/resource.go
+++ encryption-at-rest/cmd/resource/resource.go
@@ -1,123 +1,139 @@
 package resource
 
 import (
+	"context"
 	"errors"
+	"fmt"
+	"log"
 
 	"github.com/aws-cloudformation/cloudformation-cli-go-plugin/cfn/handler"
+	"github.com/mongodb/go-client-mongodb-atlas/mongodbatlas"
+	"github.com/mongodb/mongodbatlas-cloudformation-resources/util"
 )
-
 
 // Create handles the Create event from the Cloudformation service.
 func Create(req handler.Request, prevModel *Model, currentModel *Model) (handler.ProgressEvent, error) {
-    // Add your code here:
-    // * Make API calls (use req.Session)
-    // * Mutate the model
-    // * Check/set any callback context (req.CallbackContext / response.CallbackContext)
+	client, err := util.CreateMongoDBClient(*currentModel.ApiKeys.PublicKey.Value(), *currentModel.ApiKeys.PrivateKey.Value())
+	if err != nil {
+		return handler.ProgressEvent{}, err
+	}
 
-    /*
-        // Construct a new handler.ProgressEvent and return it
-        response := handler.ProgressEvent{
-            OperationStatus: handler.Success,
-            Message: "Create complete",
-            ResourceModel: currentModel,
-        }
+	projectID := *currentModel.ProjectId.Value()
 
-        return response, nil
-    */
+	encryptionAtRest := &mongodbatlas.EncryptionAtRest{
+		AwsKms: mongodbatlas.AwsKms{
+			Enabled:             currentModel.AwsKms.Enabled.Value(),
+			AccessKeyID:         *currentModel.AwsKms.AccessKeyID.Value(),
+			SecretAccessKey:     *currentModel.AwsKms.SecretAccessKey.Value(),
+			CustomerMasterKeyID: *currentModel.AwsKms.CustomerMasterKeyID.Value(),
+			Region:              *currentModel.AwsKms.Region.Value(),
+		},
+		GroupID: *currentModel.ProjectId.Value(),
+	}
 
-    // Not implemented, return an empty handler.ProgressEvent
-    // and an error
-    return handler.ProgressEvent{}, errors.New("Not implemented: Create")
+	log.Printf("Arguments: Project ID: %s, Request %#+v", projectID, encryptionAtRest)
+
+	_, _, err = client.EncryptionsAtRest.Create(context.Background(), encryptionAtRest)
+
+	if err != nil {
+		return handler.ProgressEvent{}, fmt.Errorf("error creating encryption at rest: %s", err)
+	}
+
+	return handler.ProgressEvent{
+		OperationStatus: handler.Success,
+		Message:         "Create Complete",
+		ResourceModel:   currentModel,
+	}, nil
 }
 
 // Read handles the Read event from the Cloudformation service.
 func Read(req handler.Request, prevModel *Model, currentModel *Model) (handler.ProgressEvent, error) {
-    // Add your code here:
-    // * Make API calls (use req.Session)
-    // * Mutate the model
-    // * Check/set any callback context (req.CallbackContext / response.CallbackContext)
+	// Add your code here:
+	// * Make API calls (use req.Session)
+	// * Mutate the model
+	// * Check/set any callback context (req.CallbackContext / response.CallbackContext)
 
-    /*
-        // Construct a new handler.ProgressEvent and return it
-        response := handler.ProgressEvent{
-            OperationStatus: handler.Success,
-            Message: "Read complete",
-            ResourceModel: currentModel,
-        }
+	/*
+	   // Construct a new handler.ProgressEvent and return it
+	   response := handler.ProgressEvent{
+	       OperationStatus: handler.Success,
+	       Message: "Read complete",
+	       ResourceModel: currentModel,
+	   }
 
-        return response, nil
-    */
+	   return response, nil
+	*/
 
-    // Not implemented, return an empty handler.ProgressEvent
-    // and an error
-    return handler.ProgressEvent{}, errors.New("Not implemented: Read")
+	// Not implemented, return an empty handler.ProgressEvent
+	// and an error
+	return handler.ProgressEvent{}, errors.New("Not implemented: Read")
 }
 
 // Update handles the Update event from the Cloudformation service.
 func Update(req handler.Request, prevModel *Model, currentModel *Model) (handler.ProgressEvent, error) {
-    // Add your code here:
-    // * Make API calls (use req.Session)
-    // * Mutate the model
-    // * Check/set any callback context (req.CallbackContext / response.CallbackContext)
+	// Add your code here:
+	// * Make API calls (use req.Session)
+	// * Mutate the model
+	// * Check/set any callback context (req.CallbackContext / response.CallbackContext)
 
-    /*
-        // Construct a new handler.ProgressEvent and return it
-        response := handler.ProgressEvent{
-            OperationStatus: handler.Success,
-            Message: "Update complete",
-            ResourceModel: currentModel,
-        }
+	/*
+	   // Construct a new handler.ProgressEvent and return it
+	   response := handler.ProgressEvent{
+	       OperationStatus: handler.Success,
+	       Message: "Update complete",
+	       ResourceModel: currentModel,
+	   }
 
-        return response, nil
-    */
+	   return response, nil
+	*/
 
-    // Not implemented, return an empty handler.ProgressEvent
-    // and an error
-    return handler.ProgressEvent{}, errors.New("Not implemented: Update")
+	// Not implemented, return an empty handler.ProgressEvent
+	// and an error
+	return handler.ProgressEvent{}, errors.New("Not implemented: Update")
 }
 
 // Delete handles the Delete event from the Cloudformation service.
 func Delete(req handler.Request, prevModel *Model, currentModel *Model) (handler.ProgressEvent, error) {
-    // Add your code here:
-    // * Make API calls (use req.Session)
-    // * Mutate the model
-    // * Check/set any callback context (req.CallbackContext / response.CallbackContext)
+	// Add your code here:
+	// * Make API calls (use req.Session)
+	// * Mutate the model
+	// * Check/set any callback context (req.CallbackContext / response.CallbackContext)
 
-    /*
-        // Construct a new handler.ProgressEvent and return it
-        response := handler.ProgressEvent{
-            OperationStatus: handler.Success,
-            Message: "Delete complete",
-            ResourceModel: currentModel,
-        }
+	/*
+	   // Construct a new handler.ProgressEvent and return it
+	   response := handler.ProgressEvent{
+	       OperationStatus: handler.Success,
+	       Message: "Delete complete",
+	       ResourceModel: currentModel,
+	   }
 
-        return response, nil
-    */
+	   return response, nil
+	*/
 
-    // Not implemented, return an empty handler.ProgressEvent
-    // and an error
-    return handler.ProgressEvent{}, errors.New("Not implemented: Delete")
+	// Not implemented, return an empty handler.ProgressEvent
+	// and an error
+	return handler.ProgressEvent{}, errors.New("Not implemented: Delete")
 }
 
 // List handles the List event from the Cloudformation service.
 func List(req handler.Request, prevModel *Model, currentModel *Model) (handler.ProgressEvent, error) {
-    // Add your code here:
-    // * Make API calls (use req.Session)
-    // * Mutate the model
-    // * Check/set any callback context (req.CallbackContext / response.CallbackContext)
+	// Add your code here:
+	// * Make API calls (use req.Session)
+	// * Mutate the model
+	// * Check/set any callback context (req.CallbackContext / response.CallbackContext)
 
-    /*
-        // Construct a new handler.ProgressEvent and return it
-        response := handler.ProgressEvent{
-            OperationStatus: handler.Success,
-            Message: "List complete",
-            ResourceModel: currentModel,
-        }
+	/*
+	   // Construct a new handler.ProgressEvent and return it
+	   response := handler.ProgressEvent{
+	       OperationStatus: handler.Success,
+	       Message: "List complete",
+	       ResourceModel: currentModel,
+	   }
 
-        return response, nil
-    */
+	   return response, nil
+	*/
 
-    // Not implemented, return an empty handler.ProgressEvent
-    // and an error
-    return handler.ProgressEvent{}, errors.New("Not implemented: List")
+	// Not implemented, return an empty handler.ProgressEvent
+	// and an error
+	return handler.ProgressEvent{}, errors.New("Not implemented: List")
 }

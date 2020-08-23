89e12748cdb97e76fb7d77129994c677864b0e88
Jason Mimick <jmimick@gmail.com>
2020-08-23 09:15:32 -0400

    local docker builds

--- cfn-resources/util/util.go
+++ cfn-resources/util/util.go
@@ -2,25 +2,25 @@ package util
 
 import (
 	"github.com/Sectorbob/mlab-ns2/gae/ns/digest"
-	matlasClient "github.com/mongodb/go-client-mongodb-atlas/mongodbatlas"
+	"github.com/mongodb/go-client-mongodb-atlas/mongodbatlas"
 )
 
 const (
-	// ProviderVersion is set during the release process to the release version of the binary
 	Version = "beta"
 )
 
-func CreateMongoDBClient(publicKey, privateKey string) (*matlasClient.Client, error) {
+func CreateMongoDBClient(publicKey, privateKey string) (*mongodbatlas.Client, error) {
 	// setup a transport to handle digest
 	transport := digest.NewTransport(publicKey, privateKey)
 
 	// initialize the client
 	client, err := transport.Client()
-    client.UserAgent = "mongodbatlas-cloudformation-resources/" + Version
 	if err != nil {
 		return nil, err
 	}
 
 	//Initialize the MongoDB Atlas API Client.
-	return matlasClient.NewClient(client), nil
+    atlas := mongodbatlas.NewClient(client)
+    atlas.UserAgent = "mongodbatlas-cloudformation-resources/" + Version
+	return atlas, nil
 }

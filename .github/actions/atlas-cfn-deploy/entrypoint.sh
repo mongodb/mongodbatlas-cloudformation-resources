1b7d97b93bc18dd7f291719f8bd8ca6b3655d580
admin <33664051+martinstibbe@users.noreply.github.com>
2022-09-19 19:35:39 -0500

    Fix Pull request merge to master workflow

--- .github/actions/atlas-cfn-deploy/entrypoint.sh
+++ .github/actions/atlas-cfn-deploy/entrypoint.sh
@@ -12,6 +12,16 @@ set -x
 echo "#############################################################"
 env
 #exec "$@"
+mkdir -p ~/.aws
+touch ~/.aws/credentials
+echo "[711489243244_AdministratorAccess]
+aws_access_key_id = ${{ AWS_ACCESS_KEY_ID }}
+aws_secret_access_key = ${{ AWS_SECRET_ACCESS_KEY }}
+region = ${{INPUT_AWS_DEFAULT_REGION}}" > ~/.aws/credentials
+touch ~/.aws/config
+echo "[profile 711489243244_AdministratorAccess]
+region = ${{INPUT_AWS_DEFAULT_REGION}}
+output = json " > ~/.aws/config
 cd cfn-resources
 BUILD_ONLY=1 CFN_FLAGS="--verbose --set-default --region ${INPUT_AWS_DEFAULT_REGION}" ./cfn-submit-helper.sh
 cat project/rpdk.log

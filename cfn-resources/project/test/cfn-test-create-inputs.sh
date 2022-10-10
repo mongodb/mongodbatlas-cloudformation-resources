1e060056fd9a26fcb40f10fd19a75987888e7a54
govardhan <inboxgovardhan@gmail.com>
2022-10-10 11:23:40 -0500

    cfn test changes

--- cfn-resources/project/test/cfn-test-create-inputs.sh
+++ cfn-resources/project/test/cfn-test-create-inputs.sh
@@ -22,10 +22,13 @@ mkdir inputs
 #create apikey
 org_id="$ATLAS_ORG_ID"
 echo "$org_id"
-mongocli iam org apikey create --orgId "${org_id}" --desc "cfn-test-bot3" --role ORG_MEMBER > orgid_key.json
-api_key_id=$(cat orgid_key.json | jq -r '.id')
+api_key_id=$(mongocli iam org apikey create --orgId "${org_id}" --desc "cfn-test-bot" --role ORG_MEMBER --output json | jq -r '.id')
 echo "$api_key_id"
-rm -rf orgid_key.json
+
+#create team
+team_id=$(mongocli iam team create "$team_name" --username "${project_user_name}" --orgId "${org_id}" --output json | jq -r '.id')
+echo "$team_id"
+
 
 name="${1}"
 jq --arg pubkey "$ATLAS_PUBLIC_KEY" \
@@ -35,20 +38,22 @@ jq --arg pubkey "$ATLAS_PUBLIC_KEY" \
    '.OrgId?|=$org | .ApiKeys.PublicKey?|=$pubkey | .ApiKeys.PrivateKey?|=$pvtkey | .Name?|=$name' \
    "$(dirname "$0")/inputs_1_create.template.json" > "inputs/inputs_1_create.json"
 
-name="${name}- more B@d chars !@(!(@====*** ;;::"
 jq --arg pubkey "$ATLAS_PUBLIC_KEY" \
    --arg pvtkey "$ATLAS_PRIVATE_KEY" \
    --arg org "$ATLAS_ORG_ID" \
-   --arg name "$name" \
+   --arg name "${name}- more B@d chars !@(!(@====*** ;;::" \
    '.OrgId?|=$org | .ApiKeys.PublicKey?|=$pubkey | .ApiKeys.PrivateKey?|=$pvtkey | .Name?|=$name' \
    "$(dirname "$0")/inputs_1_invalid.template.json" > "inputs/inputs_1_invalid.json"
 
 jq --arg pubkey "$ATLAS_PUBLIC_KEY" \
    --arg pvtkey "$ATLAS_PRIVATE_KEY" \
    --arg org "$ATLAS_ORG_ID" \
-   --arg name "$name" \
+   --arg name "${name}" \
    --arg key_id "$api_key_id" \
-   '.OrgId?|=$org | .ApiKeys.PublicKey?|=$pubkey | .ApiKeys.PrivateKey?|=$pvtkey | .Name?|=$name | .ProjectApiKeys[] | select(.Key=$key_id)' \
+   --arg team_id "$team_id" \
+   '.OrgId?|=$org | .ApiKeys.PublicKey?|=$pubkey | .ApiKeys.PrivateKey?|=$pvtkey | .Name?|=$name | .ProjectApiKeys[0].Key?|=$key_id | .ProjectTeams[0].TeamId?|=$team_id' \
    "$(dirname "$0")/inputs_1_update.template.json" > "inputs/inputs_1_update.json"
 
 ls -l inputs
+
+echo "TODO: Delete the team and api_key created above"

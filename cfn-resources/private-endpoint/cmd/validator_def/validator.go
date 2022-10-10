aee3272f3c489dd19d012d577e1b2c9054412778
govardhan <inboxgovardhan@gmail.com>
2022-10-10 14:47:53 -0500

    INTMDB-412 : Added privateendpoint resource

---
+++ cfn-resources/private-endpoint/cmd/validator_def/validator.go
@@ -0,0 +1,25 @@
+package validator_def
+
+type ModelValidator struct{}
+
+var CreateRequiredFields = []string{"GroupId", "Region", "ApiKeys.PublicKey", "ApiKeys.PrivateKey"}
+var ReadRequiredFields = []string{"GroupId", "Id", "ApiKeys.PublicKey", "ApiKeys.PrivateKey"}
+var UpdateRequiredFields []string
+var DeleteRequiredFields = []string{"GroupId", "Id", "ApiKeys.PublicKey", "ApiKeys.PrivateKey"}
+var ListRequiredFields = []string{"GroupId", "ApiKeys.PublicKey", "ApiKeys.PrivateKey"}
+
+func (m ModelValidator) GetCreateFields() []string {
+	return CreateRequiredFields
+}
+func (m ModelValidator) GetReadFields() []string {
+	return ReadRequiredFields
+}
+func (m ModelValidator) GetUpdateFields() []string {
+	return UpdateRequiredFields
+}
+func (m ModelValidator) GetDeleteFields() []string {
+	return DeleteRequiredFields
+}
+func (m ModelValidator) GetListFields() []string {
+	return ListRequiredFields
+}

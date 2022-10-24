530917dbdc7e58ab7e0a169182b409284ca7faa3
govardhan <inboxgovardhan@gmail.com>
2022-10-24 15:26:41 -0500

    Adding teams and keys to project creation

--- cfn-resources/project/test/cfn-test-create-inputs.sh
+++ cfn-resources/project/test/cfn-test-create-inputs.sh
@@ -20,10 +20,11 @@ mkdir inputs
 
 #create apikey
 org_id="$ATLAS_ORG_ID"
-team_name="${CFN_TEST_TAG}"
 
+random_str=$(openssl rand -hex 3)
 if [ -z "$API_KEY_ID" ]; then
-  api_key_id=$(mongocli iam org apikey create --orgId "${ATLAS_ORG_ID}" --desc "${CFN_TEST_TAG}" --role ORG_MEMBER --output json | jq -r '.id')
+  random_str=$(openssl rand -hex 3)
+api_key_id=$(mongocli iam project apikey create --desc "${CFN_TEST_TAG}" --role GROUP_OWNER --output json | jq -r '.id')
 else
   api_key_id="$API_KEY_ID"
 fi
@@ -31,7 +32,7 @@ fi
 #create team
 export user_name=$(mongocli iam project users list --output json | jq -r '.[0].emailAddress')
 if [ -z "$TEAM_ID" ]; then
-  team_id=$(mongocli iam team create "${team_name}" --username "${user_name}" --orgId "${org_id}" --output json | jq -r '.id')
+  team_id=$(mongocli iam team create "cfn-boto-${random_str}" --username "${user_name}" --orgId "${org_id}" --output json | jq -r '.id')
 else
   team_id="$TEAM_ID"
 fi
@@ -42,7 +43,9 @@ jq --arg pubkey "$ATLAS_PUBLIC_KEY" \
    --arg pvtkey "$ATLAS_PRIVATE_KEY" \
    --arg org "$ATLAS_ORG_ID" \
    --arg name "$name" \
-   '.OrgId?|=$org | .ApiKeys.PublicKey?|=$pubkey | .ApiKeys.PrivateKey?|=$pvtkey | .Name?|=$name' \
+   --arg key_id "$api_key_id" \
+   --arg team_id "$team_id" \
+   '.OrgId?|=$org | .ApiKeys.PublicKey?|=$pubkey | .ApiKeys.PrivateKey?|=$pvtkey | .Name?|=$name | .ProjectApiKeys[0].Key?|=$key_id | .ProjectTeams[0].TeamId?|=$team_id' \
    "$(dirname "$0")/inputs_1_create.template.json" > "inputs/inputs_1_create.json"
 
 jq --arg pubkey "$ATLAS_PUBLIC_KEY" \
@@ -58,7 +61,7 @@ jq --arg pubkey "$ATLAS_PUBLIC_KEY" \
    --arg name "${name}" \
    --arg key_id "$api_key_id" \
    --arg team_id "$team_id" \
-   '.OrgId?|=$org | .ApiKeys.PublicKey?|=$pubkey | .ApiKeys.PrivateKey?|=$pvtkey | .Name?|=$name | .ProjectApiKeys[0].Key?|=$key_id | .ProjectTeams[0].TeamId?|=$team_id' \
+   '.OrgId?|=$org | .ApiKeys.PublicKey?|=$pubkey | .ApiKeys.PrivateKey?|=$pvtkey | .Name?|=$name | .ProjectApiKeys[0].Key?|=$key_id | .ProjectTeams[0].TeamId?|=$team_id'\
    "$(dirname "$0")/inputs_1_update.template.json" > "inputs/inputs_1_update.json"
 
 ls -l inputs

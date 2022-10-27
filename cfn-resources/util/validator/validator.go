6c0f741f0db31a7f203bf29f019567063ad93f5d
Parthasarathy Varadhan <parthasarathy.varadhan@peerislands.io>
2022-10-27 12:03:12 -0500

    removing stale interface

--- cfn-resources/util/validator/validator.go
+++ cfn-resources/util/validator/validator.go
@@ -9,14 +9,6 @@ import (
 	"strings"
 )
 
-type ValidatorDefinition interface {
-	GetCreateFields() []string
-	GetReadFields() []string
-	GetUpdateFields() []string
-	GetDeleteFields() []string
-	GetListFields() []string
-}
-
 func ValidateModel(fields []string, model interface{}) *handler.ProgressEvent {
 
 	requiredFields := ""

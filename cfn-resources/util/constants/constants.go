c7a6bb272304acb29276a16de1900844b453c332
Dharmendran <dharmendran.rajendran@peerislands.io>
2022-11-16 00:44:06 +0530

    Update constants.go

--- cfn-resources/util/constants/constants.go
+++ cfn-resources/util/constants/constants.go
@@ -3,17 +3,26 @@ package constants
 type Event string
 
 const (
-	PubKey                 = "ApiKeys.PublicKey"
-	PvtKey                 = "ApiKeys.PrivateKey"
-	OrgID                  = "OrgId"
-	Name                   = "Name"
-	ID                     = "Id"
-	ProjectID              = "ProjectId"
-	AccepterRegionName     = "AccepterRegionName"
-	AwsAccountID           = "AwsAccountId"
-	RouteTableCIDRBlock    = "RouteTableCIDRBlock"
-	AWS                    = "AWS"
-	VPCID                  = "VpcId"
+	PubKey              = "ApiKeys.PublicKey"
+	PvtKey              = "ApiKeys.PrivateKey"
+	OrgID               = "OrgId"
+	Name                = "Name"
+	ID                  = "Id"
+	ProjectID           = "ProjectId"
+	AccepterRegionName  = "AccepterRegionName"
+	AwsAccountID        = "AwsAccountId"
+	RouteTableCIDRBlock = "RouteTableCIDRBlock"
+	AWS                 = "AWS"
+	VPCID               = "VpcId"
+
+	RegionName     = "RegionName"
+	AtlasCIDRBlock = "AtlasCidrBlock"
+
+	DatabaseName = "DatabaseName"
+	Username     = "Username"
+	Roles        = "Roles"
+	AccessList   = "AccessList"
+
 	CustomMasterKey        = "AwsKms.CustomerMasterKeyID"
 	RoleID                 = "AwsKms.RoleID"
 	Region                 = "AwsKms.Region"

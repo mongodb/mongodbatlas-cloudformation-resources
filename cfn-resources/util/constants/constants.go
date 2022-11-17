ab665484b18bdae2bee56887d46e0adb898915f5
Parthasarathy Varadhan <parthasarathy.varadhan@peerislands.io>
2022-11-17 10:28:10 -0500

    Testing AWS Role creation through CFN

--- cfn-resources/util/constants/constants.go
+++ cfn-resources/util/constants/constants.go
@@ -1,11 +1,17 @@
 package constants
 
-type Event string
+type Event any
 
 const (
-	PubKey = "ApiKeys.PublicKey"
-	PvtKey = "ApiKeys.PrivateKey"
-	OrgID  = "OrgId"
-	Name   = "Name"
-	ID     = "Id"
+	PubKey              = "ApiKeys.PublicKey"
+	PvtKey              = "ApiKeys.PrivateKey"
+	OrgID               = "OrgId"
+	Name                = "Name"
+	ID                  = "Id"
+	AwsProviderName     = "AWS"
+	TypeName            = "AWS::IAM::Role"
+	AwsRolePrefix       = "mongodbatlas-role-"
+	RolePolicyJsonGoSdk = "{\"Version\":\"2012-10-17\",\"Statement\":[{\"Effect\":\"Allow\",\"Principal\":{\"AWS\":\"$ATLAS_AWS_ACCOUNT_ARN\"}" +
+		",\"Action\":\"sts:AssumeRole\",\"Condition\":{\"StringEquals\":{\"sts:ExternalId\":\"$ATLAS_ASSUMEDROLE_EXTERNAL_ID\"}}}]}\n"
+	RolePolicyJson = "{\"AssumeRolePolicyDocument\":{\"Version\":\"2012-10-17\",\"Statement\":[{\"Effect\":\"Allow\",\"Principal\":{\"AWS\":\"$ATLAS_AWS_ACCOUNT_ARN\"},\"Action\":\"sts:AssumeRole\",\"Condition\":{\"StringEquals\":{\"sts:ExternalId\":\"$ATLAS_ASSUMEDROLE_EXTERNAL_ID\"}}}]},\"RoleName\":\"$ROLE_NAME\"}"
 )

dfcbfe3977e3736172e816c46c5b5583c7999df7
govardhan <inboxgovardhan@gmail.com>
2022-12-08 19:19:15 +0530

    initiating list with empty array instead of nil, required for cfn test

--- cfn-resources/network-peering/cmd/resource/resource.go
+++ cfn-resources/network-peering/cmd/resource/resource.go
@@ -286,7 +286,7 @@ func List(req handler.Request, prevModel *Model, currentModel *Model) (handler.P
 			resp.Response), nil
 	}
 
-	var models []interface{}
+	models := make([]interface{}, 0)
 	for i := range peerResponse {
 		var model Model
 		model.AccepterRegionName = &peerResponse[i].AccepterRegionName

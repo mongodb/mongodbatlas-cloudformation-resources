d77ae7e64ae0ee901eeca810f2a1cda9e8b31f36
Parthasarathy Varadhan <parthasarathy.varadhan@mongodb.com>
2022-12-13 16:01:42 -0500

    x509 build changes for multi region

--- cfn-resources/cfn-testing-helper.sh
+++ cfn-resources/cfn-testing-helper.sh
@@ -99,12 +99,13 @@ fi
 echo "${PROJECT_NAME}"
 #if false; then
 
-chmod +x ./test/cfn-test-create-inputs.sh
+
 
 for res in ${resources};
 do
     [[ "${_DRY_RUN}" == "true" ]] && echo "[dry-run] would have run ./test/cfn-test-create-inputs.sh for:${resource}" && continue
     cd "${res}"
+    chmod +x ./test/cfn-test-create-inputs.sh
     if [[ "${res}" == "network-peering" ]]; then
         #
         AWS_ACCOUNT_ID="${AWS_ACCOUNT_ID:-711489243244}"

c778b7d9340e20248cd775c14bf948e2c45cb3d7
Adelmar92 <adelmar92@gmail.com>
2022-12-15 18:17:55 -0500

    Added test files

---
+++ cfn-resources/ldap-verify/test/Ldap-Verify.create-sample-cfn-request.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+# project.create-sample-cfn-request.sh
+#
+# This tool generates text for a `cfn invoke` request json message.
+#
+
+set -o errexit
+set -o nounset
+#!/usr/bin/env bash
+# project.create-sample-cfn-request.sh
+#
+# This tool generates text for a `cfn invoke` request json message.
+#
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+projectId="${1}"
+
+jq --arg pubkey "$ATLAS_PUBLIC_KEY" \
+   --arg pvtkey "$ATLAS_PRIVATE_KEY" \
+   --arg groupID "$projectId" \
+   '.desiredResourceState.GroupId?|=$groupID | .desiredResourceState.ApiKeys.PublicKey?|=$pubkey | .desiredResourceState.ApiKeys.PrivateKey?|=$pvtkey' \
+   "$(dirname "$0")/Ldap-Verify.sample-cfn-request.json"
---
+++ cfn-resources/ldap-verify/test/cfn-test-create-inputs.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+# cfn-test-create-inputs.sh
+#
+# This tool generates json files in the inputs/ for `cfn test`.
+#
+
+set -o errexit
+set -o nounset
+set -o pipefail
+set -x
+
+function usage {
+    echo "usage:$0 <project_name>"
+}
+
+if [ "$#" -ne 1 ]; then usage; fi
+if [[ "$*" == help ]]; then usage; fi
+
+rm -rf inputs
+mkdir inputs
+
+#project_id
+projectName="${1}"
+
+projectId=$(mongocli iam projects list --output json | jq --arg NAME "${projectName}" -r '.results[] | select(.name==$NAME) | .id')
+if [ -z "$projectId" ]; then
+    projectId=$(mongocli iam projects create "${projectName}" --output=json | jq -r '.id')
+
+    echo -e "Created project \"${projectName}\" with id: ${projectId}\n"
+else
+    echo -e "FOUND project \"${projectName}\" with id: ${projectId}\n"
+fi
+jq --arg pubkey "$ATLAS_PUBLIC_KEY" \
+   --arg pvtkey "$ATLAS_PRIVATE_KEY" \
+   --arg group_id "$projectId" \
+   '.GroupId?|=$group_id | .ApiKeys.PublicKey?|=$pubkey | .ApiKeys.PrivateKey?|=$pvtkey' \
+   "$(dirname "$0")/inputs_1_create.template.json" > "inputs/inputs_1_create.json"
+
+jq --arg pubkey "$ATLAS_PUBLIC_KEY" \
+   --arg pvtkey "$ATLAS_PRIVATE_KEY" \
+   --arg group_id "$projectId" \
+   '.GroupId?|=$group_id | .ApiKeys.PublicKey?|=$pubkey | .ApiKeys.PrivateKey?|=$pvtkey' \
+   "$(dirname "$0")/inputs_1_invalid.template.json" > "inputs/inputs_1_invalid.json"
+
+ls -l inputs

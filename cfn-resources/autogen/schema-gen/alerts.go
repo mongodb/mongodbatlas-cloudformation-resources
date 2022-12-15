621ba4b4a7023102a01b8fbaed99e670de66c8f3
govardhan <inboxgovardhan@gmail.com>
2022-12-16 00:31:07 +0530

    remove keys

--- cfn-resources/autogen/schema-gen/alerts.go
+++ cfn-resources/autogen/schema-gen/alerts.go
@@ -10,7 +10,7 @@ func sendMail(resourceName string, changes string) {
 	m := mail.NewMessage()
 
 	// Set E-Mail sender
-	m.SetHeader("From", "govardhan.pagidi@peerislands.com")
+	m.SetHeader("From", "inboxgovardhan@gmail.com")
 
 	// Set E-Mail receivers
 	m.SetHeader("To", "govardhan.pagidi@mongodb.com")
@@ -22,7 +22,7 @@ func sendMail(resourceName string, changes string) {
 	m.SetBody("text/plain", changes)
 
 	// Settings for SMTP server
-	d := mail.NewDialer("email-smtp.ap-northeast-1.amazonaws.com", 587, "AKIA2LKAQJBWFIOIRRWB", "BMjLdK8mgP7ORnCZXpNhbqtpsjaOkXN2fmD43SChEgy8")
+	d := mail.NewDialer("smtp.gmail.com", 587, "inboxgovardhan@gmail.com", "")
 
 	// This is only needed when SSL/TLS certificate is not valid on server.
 	// In production this should be set to false.

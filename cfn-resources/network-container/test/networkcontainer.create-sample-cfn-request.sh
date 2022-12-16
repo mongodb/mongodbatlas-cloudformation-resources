638a2fe2f394b56f79ab2d3083d2135ea7ce8425
Govardhan Pagidi <govardhan.pagidi@mongodb.com>
2022-12-16 22:05:03 +0530

    INTMDB-495-script for publish (#169)
    * publish changes
    
    * script changes intermidiate push
    
    * script changes after successful publish
    
    * script changes for aws document
    
    * added submit file
    
    * added submit file
    
    * Use this file in SSM Document
    
    * Using Testinghelper and submit helper in SSM
    
    * Using Helper in SSM
    
    * Using this script in SSM
    
    * removing version in publish helper
    
    * removing unbound variable
    
    * CFN publish in one file
    
    * using cfn publish.sh
    
    * script changes for publish
    
    * changing atlas cli
    
    * deleting inputs cli change
    
    * deleting project before using it for cfn test if exists
    
    * script changes for cfn test
    
    * script changes for cfn test
    
    * script changes for cfn test
    
    * script changes for cfn test
    
    * cfn-test-create-inputs correction for
    cluster
    network-container
    network-peering
    Database User
    Using Latest Atlas cli to create Project
    
    * changing mongocli to atlas cli
    
    * Connected with Dharma and fixed enc role
    
    * removing randomness for the publish bucket since it is creating many buckets and touching hardlimits of AWS
    
    * exit 1 if the status is failed in test type
    
    * removing list support for project since it invloves many get operations
    
    * removing list support for project since it invloves many get operations
    
    * adding exit on err at publish file
    
    * remove region
    
    * script change
    
    * script change
    
    * Error breaks for every phases
    
    * Changes needed for Network Container for Publishing into Multi regions
    
    * Command change for public ListTypes
    
    * adding delay before test status loop
    
    * adding delay before test status loop
    
    * using AWS default region for private endpoint
    
    * handling test-type status for NOT_TESTED status
    
    * aws account is updated
    
    * initiating list with empty array instead of nil, required for cfn test
    
    * Aligning EncryptionAtRest resource to support. Multi region deployment
    
    * Multiregion deployment for encryption at rest
    
    * Should Check delete script.
    
    * encryption script changes
    
    * adding empty array for list operations
    
    * chanign file permissions
    
    * script changes for privatelink and cloudbackupsnapshot
    
    * Cloud-bacup-snapshot automation for deployment
    
    * bug fix
    
    * Cloud-backup-snapshot automation for deployment
    
    * create cluster dynaically, needed for snapshot deployment across regions
    
    * create cluster dynaically, needed for snapshot deployment across regions
    
    * create cluster dynaically, needed for snapshot deployment across regions
    
    * create cluster dynaically, needed for snapshot deployment across regions
    
    * script change for cloud backup restore job
    
    * bucket name change
    
    Co-authored-by: Parthasarathy <psvpartha@gmail.com>
    Co-authored-by: Parthasarathy Varadhan <parthasarathy.varadhan@mongodb.com>
    Co-authored-by: Dharmendran <dharmendran.rajendran@peerislands.io>

--- cfn-resources/network-container/test/networkcontainer.create-sample-cfn-request.sh
+++ cfn-resources/network-container/test/networkcontainer.create-sample-cfn-request.sh
@@ -8,19 +8,12 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-
-function usage {
-    echo "usage:$0 <projectId>"
-    exit 1
-}
-
 if [ "$#" -ne 1 ]; then usage; fi
 if [[ "$*" == help ]]; then usage; fi
 
-projectId="${1}"
 
-jq --arg pubkey "$ATLAS_PUBLIC_KEY" \
-   --arg pvtkey "$ATLAS_PRIVATE_KEY" \
-   --arg projectId "$projectId" \
-   '.desiredResourceState.properties.ApiKeys.PublicKey?|=$pubkey | .desiredResourceState.properties.ApiKeys.PrivateKey?|=$pvtkey | .desiredResourceState.properties.ProjectId?|=$projectId' \
+  jq --arg pubkey "$MCLI_PUBLIC_API_KEY" \
+     --arg pvtkey "$MCLI_PRIVATE_API_KEY" \
+     --arg ProjectId "$MCLI_PROJECT_ID" \
+   '.desiredResourceState.properties.ApiKeys.PublicKey?|=$pubkey | .desiredResourceState.properties.ApiKeys.PrivateKey?|=$pvtkey | .desiredResourceState.properties.ProjectId?|=$ProjectId' \
   "$(dirname "$0")/networkcontainer.sample-cfn-request.json"

d7ebf37d359b712fce8d2e8d6bdcbaff62446471
Parthasarathy Varadhan <parthasarathy.varadhan@mongodb.com>
2022-12-19 13:50:02 -0500

    Build Changes for Multiregion deployment

--- cfn-resources/global-cluster-config/test/cfn-test-delete-inputs.sh
+++ cfn-resources/global-cluster-config/test/cfn-test-delete-inputs.sh
@@ -13,10 +13,26 @@ function usage {
 }
 
 
-projectId=$(jq -r '.GroupId' ./inputs/inputs_1_create.json)
+projectId=$(jq -r '.ProjectId' ./inputs/inputs_1_create.json)
+clusterName=$(jq -r '.ClusterName' ./inputs/inputs_1_create.json)
+
+echo $projectId
+echo $clusterName
+
+#delete Cluster
+#if atlas clusters delete "$clusterName" --projectId "${projectId}" --force
+#then
+#    echo "$clusterName cluster deletion OK"
+#else
+#    (echo "Failed cleaning cluster:$clusterName" && exit 1)
+#fi
+
+echo "Waiting for cluster to get deleted"
+sleep 1200
+
 
 #delete project
-if mongocli iam projects delete "$projectId" --force
+if atlas projects delete "$projectId" --force
 then
     echo "$projectId project deletion OK"
 else

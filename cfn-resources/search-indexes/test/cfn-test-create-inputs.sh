8eee67ceed5ae72f44b331e0f241abc4cc5a6c9c
Parthasarathy Varadhan <parthasarathy.varadhan@mongodb.com>
2022-12-20 18:13:25 -0500

    Multiregion search indexes

--- cfn-resources/search-indexes/test/cfn-test-create-inputs.sh
+++ cfn-resources/search-indexes/test/cfn-test-create-inputs.sh
@@ -3,25 +3,53 @@
 #
 # This tool generates json files in the inputs/ for `cfn test`.
 #
+
 set -o errexit
 set -o nounset
 set -o pipefail
+set -x
+
 
 function usage {
-    echo "usage:$1 <projectID> $2<Cluster_Name> $3<DB_Name> $4<Coll_Name>"
-    echo "Creates a new Search Index"
+    echo "usage:$0 <project_name>"
+    echo "Creates a new project and an Cluster for testing"
 }
 
-if [ "$#" -ne 1 ]; then usage; fi
+if [ "$#" -ne 2 ]; then usage; fi
 if [[ "$*" == help ]]; then usage; fi
 
 rm -rf inputs
 mkdir inputs
 
-projectId="${2:-625454459c4e6108393d650d}"
-db_name="${4:-testdb}"
-coll_name="${5:-testcoll}"
-cluster_name="${3:-CFNTest-Cluster-001}"
+projectName="${1}"
+projectId=$(atlas projects list --output json | jq --arg NAME "${projectName}" -r '.results[] | select(.name==$NAME) | .id')
+if [ -z "$projectId" ]; then
+    projectId=$(atlas projects create "${projectName}" --output=json | jq -r '.id')
+
+    echo -e "Created project \"${projectName}\" with id: ${projectId}\n"
+else
+    echo -e "FOUND project \"${projectName}\" with id: ${projectId}\n"
+fi
+echo -e "=====\nrun this command to clean up\n=====\nmongocli iam projects delete ${projectId} --force\n====="
+export MCLI_PROJECT_ID=$projectId
+
+ClusterName="${projectName}"
+
+clusterId=$(atlas clusters create ${ClusterName} --projectId ${projectId} --backup --provider AWS --region US_EAST_1 --members 3 --tier M10 --mdbVersion 5.0 --diskSizeGB 10 --output=json | jq -r '.id')
+sleep 1200
+echo -e "Created Cluster \"${ClusterName}\" with id: ${clusterId}\n"
+
+if [ -z "$clusterId" ]; then
+    echo -e "Error Can't find Cluster \"${ClusterName}\""
+    exit 1
+fi
+
+
+atlas clusters loadSampleData ${ClusterName} --projectId ${projectId}
+
+cluster_name=${ClusterName}
+db_name="${4:-sample_airbnb}"
+coll_name="${5:-listingsAndReviews}"
 index_name="search-$RANDOM"
 u_index_name="${index_name}"
 WORDTOREMOVE="template."
@@ -45,3 +73,4 @@ do
 done
 cd ..
 ls -l inputs
+

8cdd20b9376ef89626a6eee661a3567bbff3887b
Govardhan Pagidi <govardhan.pagidi@mongodb.com>
2022-12-27 05:43:22 +0530

    INTMDB-526-Feature federated settings role mapping (#175)
    * Federated Settings Org Role Mapping Initial Commit
    
    * Schema, CFN test changes for federated settings org role mapping
    
    * Removed example input files
    
    * Removed request json
    
    * README file changes
    
    * Added note in test input create file
    
    * Addressed PR review comments
    
    * review points
    
    Co-authored-by: Sangram.Konde <sangram.konde@peerislands.io>

---
+++ cfn-resources/federated-settings-org-role-mapping/test/cfn-test-create-inputs.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+# cfn-test-create-inputs.sh
+#
+# This tool generates json files in the inputs/ for `cfn test`.
+#
+
+# NOTE: You need to set Federation Settings Id and Orgnization Id, in order to execute this resource.
+#       You can get the Federation Settings Id on Atlas UI under the 'Manage Federation Settings' cosole
+
+set -o nounset
+set -o pipefail
+WORDTOREMOVE="template."
+function usage {
+    echo "usage:$0 <project_name>"
+}
+
+if [ "$#" -ne 1 ]; then usage; fi
+if [[ "$*" == help ]]; then usage; fi
+
+rm -rf inputs
+mkdir inputs
+
+cd "$(dirname "$0")" || exit
+for inputFile in inputs_*;
+do
+  outputFile=${inputFile//$WORDTOREMOVE/};
+  jq --arg pubkey "$MCLI_PUBLIC_API_KEY" \
+     --arg pvtkey "$MCLI_PRIVATE_API_KEY" \
+	   --arg org "$ATLAS_ORG_ID" \
+     --arg FederationSettingsId "$ATLAS_FEDERATED_SETTINGS_ID" \
+     '.FederationSettingsId?|=$FederationSettingsId | .OrgId?|=$org | .ApiKeys.PublicKey?|=$pubkey | .ApiKeys.PrivateKey?|=$pvtkey ' \
+     "$inputFile" > "../inputs/$outputFile"
+done
+cd ..
+
+ls -l inputs

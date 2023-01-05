639586d2019097668585d6a19b47eae18a7eb5d6
Dharmendran <dharmendran.rajendran@peerislands.io>
2023-01-05 11:52:30 +0530

    Update util.go

--- cfn-resources/util/util.go
+++ cfn-resources/util/util.go
@@ -2,12 +2,15 @@ package util
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"os"
 	"strings"
 
 	"github.com/Sectorbob/mlab-ns2/gae/ns/digest"
 	"github.com/aws-cloudformation/cloudformation-cli-go-plugin/cfn/logging"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/ssm"
 	"github.com/mongodb/mongodbatlas-cloudformation-resources/util/logger"
 	"go.mongodb.org/atlas/mongodbatlas"
 )
@@ -93,4 +96,59 @@ func ToStringMapE(ep any) (map[string]any, error) {
 		return eMap, err
 	}
 	return eMap, nil
+}
+
+func CreateSSMClient(session *session.Session) (*ssm.SSM, error) {
+	ssmCli := ssm.New(session)
+	return ssmCli, nil
+}
+func Put(keyID, keyValue, prefix string, session *session.Session) (*ssm.PutParameterOutput, error) {
+	ssmClient, err := CreateSSMClient(session)
+	if err != nil {
+		return nil, err
+	}
+	// transform api keys to json string
+	parameterName := buildKey(keyID, prefix)
+	parameterType := "SecureString"
+	overwrite := true
+	putParamOutput, err := ssmClient.PutParameter(&ssm.PutParameterInput{Name: &parameterName, Value: &keyValue, Type: &parameterType, Overwrite: &overwrite})
+	if err != nil {
+		return nil, err
+	}
+
+	return putParamOutput, nil
+}
+
+func Delete(keyID, prefix string, session *session.Session) (*ssm.DeleteParameterOutput, error) {
+	ssmClient, err := CreateSSMClient(session)
+	if err != nil {
+		return nil, err
+	}
+	parameterName := buildKey(keyID, prefix)
+
+	deleteParamOutput, err := ssmClient.DeleteParameter(&ssm.DeleteParameterInput{Name: &parameterName})
+	if err != nil {
+		return nil, err
+	}
+
+	return deleteParamOutput, nil
+}
+
+func Get(keyID, prefix string, session *session.Session) string {
+	ssmClient, err := CreateSSMClient(session)
+	if err != nil {
+		return ""
+	}
+	parameterName := buildKey(keyID, prefix)
+	decrypt := true
+	getParamOutput, err := ssmClient.GetParameter(&ssm.GetParameterInput{Name: &parameterName, WithDecryption: &decrypt})
+	if err != nil {
+		return ""
+	}
+	return *getParamOutput.Parameter.Value
+}
+func buildKey(keyID, storePrefix string) string {
+	// this is strictly coupled with permissions for handlers, changing this means changing permissions in handler
+	// moreover changing this might cause polution in parameter store -  be sure you know what you are doing
+	return fmt.Sprintf("%s-%s", storePrefix, keyID)
 }

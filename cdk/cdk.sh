6c32f10efe75d5b5b60f6ca3cb0525f8ee4e4f2f
Mohit Talniya <mohittalniya@gmail.com>
2023-01-18 07:05:16 -0500

    missing file from commit

--- cdk/cdk.sh
+++ cdk/cdk.sh
@@ -4,9 +4,17 @@ do
   for file in $dir/mongodb-atlas-*.json;
       do
           if [[ -f $file ]]; then
+            #echo $file
+            src=$( jq -r '.typeName' $file )
+            echo "generating for $src"
+            #mkdir -p cdk/${dir} && cd cdk/${dir}
             path=$(basename $dir)
             echo $path
+            rm -rf cdk-resources/${path}/src/*.ts
+            cdk-import cfn -l typescript -s $file -o cdk-resources/${path}/src $src
+            mv cdk-resources/${path}/src/*.ts cdk-resources/${path}/src/index.ts
             cd cdk-resources/${path}
+            npx projen new awscdk-construct --author-name "MongoDB"  --docgen true --sample-code false --name path
             rm -rf .git
             cd ../..
           fi

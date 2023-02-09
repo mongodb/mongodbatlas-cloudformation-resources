fcca7f65209df0d21d8998c3d9310170e6d734a8
andreaangiolillo <andrea.angiolillo@mongodb.com>
2023-02-09 21:19:57 +0000

    INTMDB-571:cdk l2

---
+++ cdk/l2-cdk-resources/encryption-at-rest/.projenrc.js
@@ -0,0 +1,35 @@
+const { awscdk } = require('projen');
+const { ReleaseTrigger } = require('projen/lib/release');
+const atlasEncryptionAtRest = new awscdk.AwsCdkConstructLibrary({
+  author: 'MongoDBAtlas',
+  authorAddress: 'https://mongodb.com',
+  authorName: 'MongoDBAtlas',
+  cdkVersion: '2.64.0',
+  defaultReleaseBranch: 'INTMDB-548',
+  majorVersion: 1,
+  releaseToNpm: true,
+  releaseTrigger: ReleaseTrigger.manual(),
+  docgen: true,
+  name: '@mongodbatlas-awscdk/atlas-encryption-at-rest',
+  repositoryUrl: 'https://github.com/mongodb/mongodbatlas-cloudformation-resources.git',
+  sampleCode: false,
+  keywords: ['cdk',
+    'awscdk',
+    'aws-cdk',
+    'cloudformation',
+    'cfn',
+    'extensions',
+    'constructs',
+    'cfn-resources',
+    'cloudformation-registry',
+    'l2',
+    'mongodb',
+    'atlas',
+    'encryption-at-rest'],
+  description: 'Returns and edits the Encryption at Rest using Customer Key Management configuration.',
+  stability: 'experimental',
+  deps: [
+    '@mongodbatlas-awscdk/encryption-at-rest',
+  ],
+});
+atlasEncryptionAtRest.synth();

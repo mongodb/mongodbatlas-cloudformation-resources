31ba3a900a1924b5a1774ec7fd5b5615678e15fb
Mohit Talniya <mohittalniya@gmail.com>
2023-02-13 19:27:37 +0530

    updated the projenrc

--- cdk/cdk-resources/org-invitation/.projenrc.js
+++ cdk/cdk-resources/org-invitation/.projenrc.js
@@ -6,7 +6,7 @@ const org_invitation = new awscdk.AwsCdkConstructLibrary
   authorAddress: 'https://mongodb.com',
   authorName: 'MongoDBAtlas',
   cdkVersion: '2.1.0',
-  defaultReleaseBranch: 'INTMDB-548',
+  defaultReleaseBranch: 'master',
   majorVersion: 1,
   releaseToNpm: true,
   releaseTrigger: ReleaseTrigger.manual(),

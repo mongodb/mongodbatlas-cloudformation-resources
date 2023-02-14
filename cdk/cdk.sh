9f7e9cac1e942bc20e9abb23ce859af2f6dabd6b
andreaangiolillo <andrea.angiolillo@mongodb.com>
2023-02-14 11:34:45 +0000

    Updated cdk.sh script to create l2 and l3 constructors

--- cdk/cdk.sh
+++ cdk/cdk.sh
@@ -1,56 +1,102 @@
 #!/bin/bash
-# This shell script can be use to generate one or multiple L1 Constrcuts from CFN resources.
-# How to execute:
-#   1. For all resources without arguments :  ./cdk.sh
-#   2. For specific resource with argument :  ./cdk/sh "<RESOURCE NAME>"
-#                            For example   :  ./cdk/sh "auditing"  or  ./cdk/sh "cluster"
-#
-#   3. We can run it for multiple resources, let's say we are creating for "auditing", "cluster" and  "database-user"
-#      then we can pass resource name with space separated.
-#                                          : ./cdk/sh "<RESOURCE NAME> <RESOURCE NAME> <RESOURCE NAME>"
-#                            For example   : ./cdk.sh "auditing cluster database-user"
-#
-#   In this way we can run it for one or multiple resources
 
-resources=$1
-if [[ -n "$resources" ]];
-then
-  source_dir=${resources};
-else
-  echo "Generating for all resources."
-  source_dir='*';
-  dir="../cfn-resources/$source_dir"
+# Copyright 2023 MongoDB Inc
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#!/bin/bash
+# This shell script can be use to generate one L1 | L2 | L3 CDK Construct.
+# In case of L1 constructor, the script uses the CFN template to generate the CDK resource.
+# How to execute:
+#   1. CDK L1:  ./cdk.sh "<RESOURCE NAME>" L1
+#   1. CDK L1:  ./cdk.sh "<RESOURCE NAME>" L1
+#   2. CDK L2:  ./cdk/sh "<RESOURCE NAME>" L2
+#   4. CDK L3:  ./cdk/sh "<RESOURCE NAME>" L3
+
+set -euo pipefail
+
+
+_print_usage() {
+	echo
+	echo '  ./cdk.sh   "< RESOURCE NAME >" "< L1 | L2 | L3 >"'
+  echo 'Example: ./cdk.sh cluster L1'
+}
+
+
+
+resource=$1
+cdk_type=$(echo "$2" | tr '[:lower:]' '[:upper:]')
+
+if [[ -z "${resource}" || -z "${cdk_type}" ]]; then
+    echo "Error: please provide the resource name and the type of CDK constructor"
+    _print_usage
+    exit 1
 fi
-for resource in ../cfn-resources/$source_dir;
-do
-  # echo $dir
+
+
+if [ -z "${cdk_type}" || "$cdk_type" = "L1" ]; then
+  echo "Generating L1 CDK resource"
   dir="../cfn-resources/$resource"
   for file in "$dir"/mongodb-atlas-*.json;
       do
           if [[ -f $file ]]; then
-            #echo $file
             src=$( jq -r '.typeName' "$file" )
             echo "generating for $src"
-            #mkdir -p cdk/${dir} && cd cdk/${dir}
             path=$(basename "$dir")
             echo "$path"
             index_exists=false
-             if [ -f cdk-resources/"${path}"/src/index.ts ]; then
+            if [ -f cdk-resources/"${path}"/src/index.ts ]; then
                 rm -rf cdk-resources/"${path}"/src/*.ts
                 index_exists=true
-             fi
+            fi
 
             cdk-import cfn -l typescript -s "$file" -o cdk-resources/"${path}"/src "$src"
             # need rename resource file to index.ts file
             mv "cdk-resources"/"${path}"/"src"/"mongodb-atlas-""${path//-}".ts cdk-resources/"${path}"/src/index.ts
             if [ "$index_exists" = true ] ; then
-               continue
+              continue
             fi
-            cd cdk-resources/"${path}" || exit
-            npx projen new awscdk-construct --npm-access "public" --author "MongoDBAtlas" --author-name "MongoDBAtlas" --docgen true --sample-code false --name '@mongodbatlas-awscdk/'"${path}" --author-address 'https://mongodb.com' --cdk-version '2.1.0' --default-release-branch 'INTMDB-548' --major-version 1 --release-to-npm true --repository-url 'https://github.com/mongodb/mongodbatlas-cloudformation-resources.git' --description 'Retrieves or creates '"${path}"' in any given Atlas organization' --keywords {'cdk','awscdk','aws-cdk','cloudformation','cfn','extensions','constructs','cfn-resources','cloudformation-registry','l1','mongodb','atlas',"$path"}
+            
+            pushd "cdk-resources/${path}" || exit
+            npx projen new awscdk-construct --npm-access "public" --author "MongoDBAtlas" --author-name "MongoDBAtlas" --docgen true --sample-code false --name '@mongodbatlas-awscdk/'"${path}" --author-address 'https://mongodb.com' --cdk-version '2.1.0' --default-release-branch 'master' --major-version 1 --release-to-npm true --repository-url 'https://github.com/mongodb/mongodbatlas-cloudformation-resources.git' --description 'Retrieves or creates '"${path}"' in any given Atlas organization' --keywords {'cdk','awscdk','aws-cdk','cloudformation','cfn','extensions','constructs','cfn-resources','cloudformation-registry','l1','mongodb','atlas',"$path"}
             rm -rf .git
-            cd ../.. || exit
+            popd
           fi
       done
-done
+fi
+
+if [ "$cdk_type" = "L2" ]; then
+  echo "Generating L2 CDK resource"
+
+  mkdir "l2-cdk-resources${path}"
+  pushd "l2-cdk-resources${path}"
+
+  npx projen new awscdk-construct --npm-access "public" --author "MongoDBAtlas" --author-name "MongoDBAtlas" --docgen true --sample-code false --name '@mongodbatlas-awscdk-l2/'"${path}" --author-address 'https://mongodb.com' --cdk-version '2.1.0' --default-release-branch 'master' --major-version 1 --release-to-npm true --repository-url 'https://github.com/mongodb/mongodbatlas-cloudformation-resources.git' --description 'Retrieves or creates '"${path}"' in any given Atlas organization' --keywords {'cdk','awscdk','aws-cdk','cloudformation','cfn','extensions','constructs','cfn-resources','cloudformation-registry','l2','mongodb','atlas',"$path"}
+  rm -rf .git
+
+  popd
+fi
+
+if [ "$cdk_type" = "L3" ]; then
+  echo "Generating L3 CDK resource"
+  
+  mkdir "l3-cdk-resources${path}"
+  pushd "l3-cdk-resources${path}"
+  
+  npx projen new awscdk-construct --npm-access "public" --author "MongoDBAtlas" --author-name "MongoDBAtlas" --docgen true --sample-code false --name '@mongodbatlas-awscdk-l3/'"${path}" --author-address 'https://mongodb.com' --cdk-version '2.1.0' --default-release-branch 'master' --major-version 1 --release-to-npm true --repository-url 'https://github.com/mongodb/mongodbatlas-cloudformation-resources.git' --description 'Retrieves or creates '"${path}"' in any given Atlas organization' --keywords {'cdk','awscdk','aws-cdk','cloudformation','cfn','extensions','constructs','cfn-resources','cloudformation-registry','l3','mongodb','atlas',"$path"}
+  rm -rf .git
+  
+  popd
+fi
+
 

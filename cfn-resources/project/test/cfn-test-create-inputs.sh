1d73c2526e6a6dbe992b891b04c4c27062dc7740
Andrea Angiolillo <andrea.angiolillo@mongodb.com>
2023-02-16 12:12:22 +0000

    Update script to use template folder (#310)

--- cfn-resources/project/test/cfn-test-create-inputs.sh
+++ cfn-resources/project/test/cfn-test-create-inputs.sh
@@ -1,8 +1,23 @@
-#!/usr/bin/env bash
-# cfn-test-create-inputs.sh
+#!/bin/bash
+
+# Copyright 2023 MongoDB Inc
 #
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# ./cfn-test-create-inputs.sh PROJECT_NAME
 # This tool generates json files in the inputs/ for `cfn test`.
-#
+
+set -euo pipefail
 
 function usage {
 	echo "usage:$0 <project_name>"
@@ -45,14 +60,14 @@ jq --arg pubkey "$ATLAS_PUBLIC_KEY" \
 	--arg key_id "$api_key_id" \
 	--arg team_id "$team_id" \
 	'.OrgId?|=$org | .ApiKeys.PublicKey?|=$pubkey | .ApiKeys.PrivateKey?|=$pvtkey | .Name?|=$name | .ProjectApiKeys[0].Key?|=$key_id | .ProjectTeams[0].TeamId?|=$team_id' \
-	"$(dirname "$0")/inputs_1_create.template.json" >"inputs/inputs_1_create.json"
+	"$(dirname "$0")/templates/inputs_1_create.template.json" >"inputs/inputs_1_create.json"
 
 jq --arg pubkey "$ATLAS_PUBLIC_KEY" \
 	--arg pvtkey "$ATLAS_PRIVATE_KEY" \
 	--arg org "$ATLAS_ORG_ID" \
 	--arg name "${name}- more B@d chars !@(!(@====*** ;;::" \
 	'.OrgId?|=$org | .ApiKeys.PublicKey?|=$pubkey | .ApiKeys.PrivateKey?|=$pvtkey | .Name?|=$name' \
-	"$(dirname "$0")/inputs_1_invalid.template.json" >"inputs/inputs_1_invalid.json"
+	"$(dirname "$0")/templates/inputs_1_invalid.template.json" >"inputs/inputs_1_invalid.json"
 
 jq --arg pubkey "$ATLAS_PUBLIC_KEY" \
 	--arg pvtkey "$ATLAS_PRIVATE_KEY" \
@@ -61,7 +76,7 @@ jq --arg pubkey "$ATLAS_PUBLIC_KEY" \
 	--arg key_id "$api_key_id" \
 	--arg team_id "$team_id" \
 	'.OrgId?|=$org | .ApiKeys.PublicKey?|=$pubkey | .ApiKeys.PrivateKey?|=$pvtkey | .Name?|=$name | .ProjectApiKeys[0].Key?|=$key_id | .ProjectTeams[0].TeamId?|=$team_id' \
-	"$(dirname "$0")/inputs_1_update.template.json" >"inputs/inputs_1_update.json"
+	"$(dirname "$0")/templates/inputs_1_update.template.json" >"inputs/inputs_1_update.json"
 
 ls -l inputs
 

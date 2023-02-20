3930c77e88752ed603285c90aeaae7989c931027
govardhan <inboxgovardhan@gmail.com>
2023-02-20 14:24:23 +0530

    creating projectid for test

--- cfn-resources/auditing/test/auditing.create-sample-cfn-request.sh
+++ cfn-resources/auditing/test/auditing.create-sample-cfn-request.sh
@@ -8,6 +8,15 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+projectName="${1}"
+projectId=$(atlas projects list --output json | jq --arg NAME "${projectName}" -r '.results[] | select(.name==$NAME) | .id')
+if [ -z "$projectId" ]; then
+    projectId=$(atlas projects create "${projectName}" --output=json | jq -r '.id')
+
+    echo -e "Created project \"${projectName}\" with id: ${projectId}\n"
+else
+    echo -e "FOUND project \"${projectName}\" with id: ${projectId}\n"
+fi
 
 jq --arg groupID "$projectId" \
    '.desiredResourceState.GroupId?|=$groupID' \

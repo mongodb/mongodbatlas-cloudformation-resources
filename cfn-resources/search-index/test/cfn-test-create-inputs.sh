1721510eeaebe6a117b523f4844b0b159e9dd290
andreaangiolillo <andrea.angiolillo@mongodb.com>
2023-02-21 16:52:05 +0000

    Update cfn-test-create-inputs.sh

--- cfn-resources/search-index/test/cfn-test-create-inputs.sh
+++ cfn-resources/search-index/test/cfn-test-create-inputs.sh
@@ -63,7 +63,7 @@ for inputFile in inputs_*; do
 		--arg name "$index_name" \
 		--arg db "$db_name" \
 		--arg coll "$coll_name" \
-		'.CollectionName?|=$coll |.Database?|=$db |.GroupId?|=$org |.ClusterName?|=$cluster |.Name?|=$name' \
+		'.CollectionName?|=$coll |.Database?|=$db |.ProjectId?|=$org |.ClusterName?|=$cluster |.Name?|=$name' \
 		"$inputFile" >"../inputs/$outputFile"
 done
 cd ..

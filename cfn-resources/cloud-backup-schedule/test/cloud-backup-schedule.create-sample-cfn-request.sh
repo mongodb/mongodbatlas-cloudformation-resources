3689941823c4b90401525c9ecae8ba2311db76bd
Govardhan Pagidi <govardhan.pagidi@mongodb.com>
2023-02-21 16:27:36 +0530

    INTMDB-607: cloudbackup schedule profile changes (#322)

--- cfn-resources/cloud-backup-schedule/test/cloud-backup-schedule.create-sample-cfn-request.sh
+++ cfn-resources/cloud-backup-schedule/test/cloud-backup-schedule.create-sample-cfn-request.sh
@@ -6,9 +6,12 @@
 set -o errexit
 set -o nounset
 set -o pipefail
-jq --arg pubkey "$MCLI_PUBLIC_API_KEY" \
-	--arg pvtkey "$MCLI_PRIVATE_API_KEY" \
-	--arg group_id "$PROJECT_ID" \
-	--arg ClusterName "$CLUSTER_NAME" \
-	'.ProjectId?|=$group_id | .ApiKeys.PublicKey?|=$pubkey | .ApiKeys.PrivateKey?|=$pvtkey | .ClusterName?|=$ClusterName' \
-	"$(dirname "$0")/cloud-backup-schedule.sample-cfn-request.json"
+
+
+projectId="${1}"
+clusterName="${2}"
+
+  jq --arg projectId "$projectId" \
+     --arg clusterName "$clusterName" \
+     '.desiredResourceState.ProjectId?|=$projectId | .desiredResourceState.ClusterName?|=$clusterName' \
+    "$(dirname "$0")/cloud-backup-schedule.sample-cfn-request.json"

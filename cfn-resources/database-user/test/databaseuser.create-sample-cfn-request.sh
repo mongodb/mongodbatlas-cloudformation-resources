af0e6ccebd06c88e3b07527d6f5cad499aa11b3c
Govardhan Pagidi <govardhan.pagidi@mongodb.com>
2023-02-21 05:40:39 +0530

    INTMDB-602: database user profile changes (#318)

--- cfn-resources/database-user/test/databaseuser.create-sample-cfn-request.sh
+++ cfn-resources/database-user/test/databaseuser.create-sample-cfn-request.sh
@@ -8,9 +8,10 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+
 function usage {
-	echo "usage:$0 <projectId> <databaseuser.username> <databaseuser.password>"
-	exit 1
+    echo "usage:$0 <projectId> <databaseuser.username> <databaseuser.password>"
+    exit 1
 }
 
 if [ "$#" -ne 3 ]; then usage; fi
@@ -20,10 +21,8 @@ projectId="${1}"
 username="${2}"
 password="${3}"
 
-jq --arg pubkey "$ATLAS_PUBLIC_KEY" \
-	--arg pvtkey "$ATLAS_PRIVATE_KEY" \
-	--arg projectId "$projectId" \
-	--arg username "$username" \
-	--arg password "$password" \
-	'.desiredResourceState.properties.ApiKeys.PublicKey?|=$pubkey | .desiredResourceState.properties.ApiKeys.PrivateKey?|=$pvtkey | .desiredResourceState.properties.Username?|=$username | .desiredResourceState.properties.Password?|=$password | .desiredResourceState.properties.ProjectId?|=$projectId' \
-	"$(dirname "$0")/databaseuser.sample-cfn-request.json"
+ jq --arg ProjectId "$projectId" \
+   --arg username "$username" \
+   --arg password "$password" \
+   '.desiredResourceState.properties.Username?|=$username | .desiredResourceState.properties.Password?|=$password | .desiredResourceState.properties.ProjectId?|=$projectId' \
+  "$(dirname "$0")/databaseuser.sample-cfn-request.json"

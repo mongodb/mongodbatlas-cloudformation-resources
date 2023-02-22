338bf0e5ac16fe126a858cec6f0acba53f1a4297
Aastha Mahendru <aastha.mahendru@mongodb.com>
2023-02-22 10:52:05 +0000

    formatting fix

--- cfn-resources/project-invitation/cmd/resource/resource.go
+++ cfn-resources/project-invitation/cmd/resource/resource.go
@@ -16,9 +16,9 @@ package resource
 
 import (
 	"context"
-	"github.com/aws/aws-sdk-go/aws"
 
 	"github.com/aws-cloudformation/cloudformation-cli-go-plugin/cfn/handler"
+	"github.com/aws/aws-sdk-go/aws"
 	"github.com/mongodb/mongodbatlas-cloudformation-resources/util"
 	"github.com/mongodb/mongodbatlas-cloudformation-resources/util/constants"
 	log "github.com/mongodb/mongodbatlas-cloudformation-resources/util/logger"

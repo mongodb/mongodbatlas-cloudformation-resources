ed5b74132858bb1dc4d03e877f5572526137053b
Adelmar92 <adelmar92@gmail.com>
2023-02-22 22:51:22 -0500

    fixed test inputs

--- cfn-resources/network-peering/test/cfn-test-create-inputs.sh
+++ cfn-resources/network-peering/test/cfn-test-create-inputs.sh
@@ -29,9 +29,52 @@ fi
 
 echo "Check if a project is created $projectId"
 
+echo "Creating network container"
+
+region=$AWS_DEFAULT_REGION
+if [ -z "$region" ]; then
+	region=$(aws configure get region)
+fi
+
+region_name_1=$(echo "$region" | tr '[:lower:]' '[:upper:]')
+region_name=$(echo "$region_name_1" | sed 's/-/_/g')
+public_key=$ATLAS_PUBLIC_KEY
+private_key=$ATLAS_PRIVATE_KEY
+
+# Generate a random IPv4 address
+IP=10.$(($RANDOM % 256)).$(($RANDOM % 256)).0
+CIDR=24
+
+# Generate the CIDR block
+cidr_block="$IP/$CIDR"
+
+response=$(curl --user "$public_key:$private_key" --digest --request POST \
+  --url https://cloud.mongodb.com/api/atlas/v1.0/groups/$projectId/containers \
+  --header "Content-Type: application/json" \
+  --data '{
+    "providerName": "AWS",
+    "atlasCidrBlock": "'"$cidr_block"'",
+    "regionName": "'"$region_name"'"
+  }')
+
+http_status=$(echo "$response" | jq -r '.error')
+http_message=$(echo "$response" | jq -r '.errorCode')
+
+echo "result: $response"
+echo "result: $http_status"
+
+if [[ "$http_status" != "null" ]]; then
+	echo "FAIL TO CREATE Container HTTP error: $http_status , message: $http_message"
+  exit 1
+fi
+
+container_id=$(echo $response | jq -r '.id')
+
+echo "Container ID: $container_id"
+
 awsId="${2}"
 vpcId="${3}"
-nwkConId="${NETWORK_CONTAINER_ID}"
+nwkConId="${container_id}"
 echo -e "=====\nrun this command to clean up\n=====\nmongocli iam projects delete ${projectId} --force\n====="
 
 cd "$(dirname "$0")" || exit
---
+++ cfn-resources/network-peering/test/cfn-test-delete-inputs.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+# cfn-test-delete-inputs.sh
+#
+# This tool deletes the mongodb resources used for `cfn test` as inputs.
+#
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+function usage {
+	echo "usage:$0 "
+}
+
+projectId=$(jq -r '.ProjectId' ./inputs/inputs_1_create.json)
+containerId=$(jq -r '.ContainerId' ./inputs/inputs_1_create.json)
+
+if atlas networking containers delete "$containerId" --projectId $projectId --force; then
+	echo "$projectId network container deletion OK"
+else
+	(echo "Failed cleaning project:$projectId" && exit 1)
+fi
+
+#delete project
+if atlas projects delete "$projectId" --force; then
+	echo "$projectId project deletion OK"
+else
+	(echo "Failed cleaning project:$projectId" && exit 1)
+fi

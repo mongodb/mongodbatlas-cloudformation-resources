e9a3428b96f78e7ce14e44099b60af8da3059551
Aastha Mahendru <aastha.mahendru@mongodb.com>
2023-02-27 12:18:13 +0000

    test

--- cfn-resources/cfn-install-mongo-cli.sh
+++ cfn-resources/cfn-install-mongo-cli.sh
@@ -4,14 +4,14 @@ set -xeo
 
 echo "running install script.."
 
-#sudo cat <<EOT >> /etc/yum.repos.d/mongodb-enterprise-6.0.repo
-#[mongodb-org-6.0]
-#name=MongoDB Repository
-#baseurl=https://repo.mongodb.org/yum/amazon/2/mongodb-org/6.0/x86_64/
-#gpgcheck=1
-#enabled=1
-#gpgkey=https://pgp.mongodb.com/server-6.0.asc
-#EOT
-#
-#echo "installing cli..."
-#sudo yum install -y mongodb-atlas-cli
+sudo cat <<EOT >> /etc/yum.repos.d/mongodb-enterprise-6.0.repo
+[mongodb-org-6.0]
+name=MongoDB Repository
+baseurl=https://repo.mongodb.org/yum/amazon/2/mongodb-org/6.0/x86_64/
+gpgcheck=1
+enabled=1
+gpgkey=https://pgp.mongodb.com/server-6.0.asc
+EOT
+
+echo "installing cli..."
+sudo yum install -y mongodb-atlas-cli

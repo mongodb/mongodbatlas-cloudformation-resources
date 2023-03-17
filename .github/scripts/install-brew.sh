454c7c8f9261f79db96619c30c99dcc6b6d7e1ec
andreaangiolillo <andrea.angiolillo@mongodb.com>
2023-03-17 21:27:24 +0000

    Updated workspace

--- .github/scripts/install-brew.sh
+++ .github/scripts/install-brew.sh
@@ -3,10 +3,10 @@ set eu
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 # shellcheck disable=SC2016
-(
-	echo
-	echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"'
-) >>/home/runner/.bash_profile
+test -d ~/.linuxbrew && eval "$(~/.linuxbrew/bin/brew shellenv)"
+test -d /home/linuxbrew/.linuxbrew && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+test -r ~/.bash_profile && echo "eval \"\$($(brew --prefix)/bin/brew shellenv)\"" >>~/.bash_profile
+echo "eval \"\$($(brew --prefix)/bin/brew shellenv)\"" >>~/.profile
 
 sudo apt-get install build-essential
 brew install gcc

681f09a71421e172850ed5eea38c3466a5c34303
andreaangiolillo <andrea.angiolillo@mongodb.com>
2023-03-17 21:30:45 +0000

    Updated workspace

--- .github/scripts/install-brew.sh
+++ .github/scripts/install-brew.sh
@@ -2,7 +2,7 @@
 set eu
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-# shellcheck disable=SC2016
+
 test -d ~/.linuxbrew && eval "$(~/.linuxbrew/bin/brew shellenv)"
 test -d /home/linuxbrew/.linuxbrew && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 test -r ~/.bash_profile && echo "eval \"\$($(brew --prefix)/bin/brew shellenv)\"" >>~/.bash_profile
@@ -10,3 +10,4 @@ echo "eval \"\$($(brew --prefix)/bin/brew shellenv)\"" >>~/.profile
 
 sudo apt-get install build-essential
 brew install gcc
+brew install mongodb-atlas-cli

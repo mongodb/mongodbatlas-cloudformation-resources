10275da1a0081f544594ac4294c27427eb6cfe70
Aastha Mahendru <aastha.mahendru@mongodb.com>
2023-03-23 22:36:29 +0000

    minor changes to scripts and examples

--- cfn-resources/private-endpoint/test/cfn-test-create-inputs.sh
+++ cfn-resources/private-endpoint/test/cfn-test-create-inputs.sh
@@ -1,4 +1,18 @@
 #!/usr/bin/env bash
+# Copyright 2023 MongoDB Inc
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # cfn-test-create-inputs.sh
 #
 # This tool generates json files in the inputs/ for `cfn test`.
@@ -20,14 +34,20 @@ if [[ "$*" == help ]]; then usage; fi
 rm -rf inputs
 mkdir inputs
 
-region=$AWS_DEFAULT_REGION
-if [ -z "$region" ]; then
-	region=$(aws configure get region)
+projectName="${1:-$PROJECT_NAME}"
+vpcId="${2:-$AWS_VPC_ID}"
+subnetId="${3:-$AWS_SUBNET_ID}"
+
+if ! test -v AWS_DEFAULT_REGION; then
+    region=$(aws configure get region)
+else
+  region=$AWS_DEFAULT_REGION
 fi
 
-projectName="${1}"
-vpcId="${2}"
-subnetId="${3}"
+#region="${4:-$AWS_DEFAULT_REGION}"
+#if [ -z "$region" ]; then
+#	region=$(aws configure get region)
+#fi
 
 projectId=$(atlas projects list --output json | jq --arg NAME "${projectName}" -r '.results[] | select(.name==$NAME) | .id')
 if [ -z "$projectId" ]; then

1b86b9f2cf113a7ced8237557d75636c810815a0
Aastha Mahendru <aastha.mahendru@mongodb.com>
2023-03-27 12:00:16 +0100

    add README for search-index and pe-regional CFN

--- cfn-resources/private-endpoint-regional-mode/test/private-endpoint-regional-mode.create-sample-cfn-request.sh
+++ cfn-resources/private-endpoint-regional-mode/test/private-endpoint-regional-mode.create-sample-cfn-request.sh
@@ -1,4 +1,18 @@
 #!/usr/bin/env bash
+# Copyright 2023 MongoDB Inc
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # private-endpoint-regional-mode.create-sample-cfn-request.sh
 #
 # This tool generates text for a `cfn invoke` request json message.
@@ -6,8 +20,6 @@
 set -o errexit
 set -o nounset
 set -o pipefail
-jq --arg pubkey "$MCLI_PUBLIC_API_KEY" \
-	--arg pvtkey "$MCLI_PRIVATE_API_KEY" \
-	--arg ProjectId "$MCLI_PROJECT_ID" \
-	'.desiredResourceState.ProjectId?|=$ProjectId | .desiredResourceState.ApiKeys.PublicKey?|=$pubkey | .desiredResourceState.ApiKeys.PrivateKey?|=$pvtkey' \
+jq --arg ProjectId "$ATLAS_PROJECT_ID" \
+	'.desiredResourceState.ProjectId?|=$ProjectId ' \
 	"$(dirname "$0")/private-endpoint-regional-mode.sample-cfn-request.json"

3f9fd37596acf1ba6a11d942ef3a27b241aa0948
Aastha Mahendru <aastha.mahendru@mongodb.com>
2023-03-28 00:06:09 +0100

    Remove MCLI* env variables and cleanup READMEs

--- cfn-resources/cloud-backup-snapshot/test/cfn-test-create-inputs.sh
+++ cfn-resources/cloud-backup-snapshot/test/cfn-test-create-inputs.sh
@@ -1,4 +1,18 @@
 #!/usr/bin/env bash
+# Copyright 2023 MongoDB Inc
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # cfn-test-create-inputs.sh
 #
 # This tool generates json files in the inputs/ for `cfn test`.
@@ -29,7 +43,6 @@ if [ -z "$projectId" ]; then
 	projectId=$(atlas projects create "${projectName}" --output=json | jq -r '.id')
 	echo -e "Cant find project \"${projectName}\"\n"
 fi
-export MCLI_PROJECT_ID=$projectId
 
 atlas clusters create "${clusterName}" --projectId "${projectId}" --backup --provider AWS --region US_EAST_1 --members 3 --tier M10 --mdbVersion 5.0 --diskSizeGB 10 --output=json
 atlas clusters watch "${clusterName}" --projectId "${projectId}"

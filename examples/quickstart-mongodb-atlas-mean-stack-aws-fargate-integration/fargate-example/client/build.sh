927362a987a1cca408700aa4a625dbfc06b44054
andreaangiolillo <andrea.angiolillo@mongodb.com>
2023-03-31 13:40:45 +0100

    INTMDB-594: [CFN-PI] Improve documentations for quickstarts

--- examples/quickstart-mongodb-atlas-mean-stack-aws-fargate-integration/fargate-example/client/build.sh
+++ examples/quickstart-mongodb-atlas-mean-stack-aws-fargate-integration/fargate-example/client/build.sh
@@ -10,22 +10,23 @@ account_id=$(aws sts get-caller-identity --profile ${profile} --query Account --
 
 # If the repository doesn't exist in ECR, create it.
 if ! aws ecr describe-repositories \
-	--profile "${profile}" \
-	--region "${region}" \
-	--repository-names "${repo_name}" >/dev/null 2>&1; then
-	aws ecr create-repository \
-		--profile "${profile}" \
-		--region "${region}" \
-		--repository-name "${repo_name}" >/dev/null
+    --profile "${profile}" \
+    --region "${region}" \
+    --repository-names "${repo_name}" > /dev/null 2>&1
+then
+    aws ecr create-repository \
+        --profile "${profile}" \
+        --region "${region}" \
+        --repository-name "${repo_name}" > /dev/null
 fi
 
 # Authenticate Docker to the Amazon ECR private registry.
 aws ecr get-login-password \
-	--profile "${profile}" \
-	--region "${region}" |
-	docker login \
-		--username AWS \
-		--password-stdin "${account_id}.dkr.ecr.${region}.amazonaws.com"
+    --profile "${profile}" \
+    --region "${region}" \
+| docker login \
+    --username AWS \
+    --password-stdin "${account_id}.dkr.ecr.${region}.amazonaws.com"
 
 # Build the docker image locally with the image name and then push it to ECR
 # with the full name.

563adb7ecc1af14a1010a7e96d6b4b3b3c5011bf
TSowbaranika <109083730+TSowbaranika@users.noreply.github.com>
2023-08-16 12:23:40 -0500

    Adding contract_testing

---
+++ cfn-resources/data-lake-pipeline/test/contract-testing/cfn-test-create-inputs.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+# cfn-test-create-inputs.sh
+#
+# This tool generates json files in the inputs/ for `cfn test`.
+#
+
+set -o errexit
+set -o nounset
+set -o pipefail
+set -x
+
+if [ -z "${AWS_DEFAULT_REGION+x}" ];then
+  echo "AWS_DEFAULT_REGION must be set"
+  exit 1
+fi
+
+# setting projectName
+projectName="cfn-bot-datalake-pipeline-$((1 + RANDOM % 10000))"
+
+./test/cfn-test-create-inputs.sh $projectName

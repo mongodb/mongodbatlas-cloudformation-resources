5d79be7f5e9f0435aedd0232a44334c03c7da41e
Govardhan Pagidi <govardhan.pagidi@mongodb.com>
2023-08-22 13:15:58 -0500

    Update cfn-test-create-inputs.sh

--- cfn-resources/federated-database-instance/test/cfn-test-create-inputs.sh
+++ cfn-resources/federated-database-instance/test/cfn-test-create-inputs.sh
@@ -63,7 +63,7 @@ echo "$keyRegion"
 
 
 echo -e "--------------------------------create aws bucket document starts ----------------------------\n"
-bucketName="mongodb-atlas-data-federation/cfn-test-${keyRegion}"
+bucketName="mongodb-atlas-cfn-test-df-${keyRegion}"
 aws s3 rb "s3://${bucketName}" --force
 aws s3 mb "s3://${bucketName}" --output json
 echo -e "--------------------------------create aws bucket document  ends ----------------------------\n"

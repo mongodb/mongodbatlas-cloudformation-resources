fdcac4fd354a092283714157822ddc3f0e1fc8d0
Govardhan Pagidi <govardhan.pagidi@mongodb.com>
2023-08-23 11:25:00 -0500

    Update cfn-test-delete-inputs.sh

--- cfn-resources/data-lake-pipeline/test/cfn-test-delete-inputs.sh
+++ cfn-resources/data-lake-pipeline/test/cfn-test-delete-inputs.sh
@@ -27,19 +27,15 @@ function usage {
 }
 
 projectId=$(jq -r '.ProjectId' ./inputs/inputs_1_create.json)
-pipelineName=$(jq -r '.Name' ./inputs/inputs_1_create.json)
-clusterName=$(jq -r '.Source.ClusterName' ./inputs/inputs_1_create.json)
-#delete project
-if  atlas dataLakePipelines delete "$pipelineName" --force; then
-	echo "$pipelineName project deletion OK"
-else
-	(echo "Failed cleaning datalake pipeline:$pipelineName" && exit 1)
-fi
 
+clusterName=$(jq -r '.Source.ClusterName' ./inputs/inputs_1_create.json)
+
+#delete Cluster
 atlas clusters delete "$clusterName" --projectId "${projectId}" --force
 atlas clusters watch "${clusterName}" --projectId "${projectId}"
 echo -e "Cluster Deleted \"${clusterName}\""
 
+#delete Project
 if atlas projects delete "$projectId"  -- force; then
   echo "$projectId is deleted"
 else

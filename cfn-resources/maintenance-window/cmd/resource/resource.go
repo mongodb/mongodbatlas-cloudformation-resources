4f64c5e760974d6bb3e7703c6f64fb26e589b7fe
Leo Antoli <leo.antoli@mongodb.com>
2023-09-26 09:01:41 +0200

    Create

--- cfn-resources/maintenance-window/cmd/resource/resource.go
+++ cfn-resources/maintenance-window/cmd/resource/resource.go
@@ -27,6 +27,7 @@ import (
 	"github.com/mongodb/mongodbatlas-cloudformation-resources/util/logger"
 	progress_events "github.com/mongodb/mongodbatlas-cloudformation-resources/util/progressevent"
 	"github.com/mongodb/mongodbatlas-cloudformation-resources/util/validator"
+	"go.mongodb.org/atlas-sdk/v20230201008/admin"
 	mongodbatlas "go.mongodb.org/atlas/mongodbatlas"
 )
 
@@ -36,47 +37,33 @@ func setup() {
 	util.SetupLogger("mongodb-atlas-maintenance-window")
 }
 
-func (m Model) toAtlasModel() mongodbatlas.MaintenanceWindow {
-	return mongodbatlas.MaintenanceWindow{
-		DayOfWeek:            *m.DayOfWeek,
-		HourOfDay:            m.HourOfDay,
-		StartASAP:            m.StartASAP,
-		AutoDeferOnceEnabled: m.AutoDeferOnceEnabled,
-	}
-}
-
 func Create(req handler.Request, prevModel *Model, currentModel *Model) (handler.ProgressEvent, error) {
 	setup()
-	// Validation
-	if errEvent := validator.ValidateModel(RequiredFields, currentModel); errEvent != nil {
+
+	if err := validator.ValidateModel(RequiredFields, currentModel); err != nil {
 		_, _ = logger.Warnf("Validation Error")
-		return *errEvent, nil
+		return *err, nil
 	}
 	if currentModel.Profile == nil || *currentModel.Profile == "" {
 		currentModel.Profile = aws.String(profile.DefaultProfile)
 	}
-	// Create atlas client
-	client, pe := util.NewMongoDBClient(req, currentModel.Profile)
+	client, pe := util.NewAtlasClient(&req, currentModel.Profile)
 	if pe != nil {
-		_, _ = logger.Warnf("CreateMongoDBClient error: %v", *pe)
 		return *pe, nil
 	}
 
-	maintenanceWindow, _ := get(client, *currentModel)
+	maintenanceWindow, _ := get2(client, *currentModel)
 	if maintenanceWindow != nil {
 		return progress_events.GetFailedEventByCode("resource already exists", cloudformation.HandlerErrorCodeAlreadyExists), nil
 	}
 
-	var res *mongodbatlas.Response
-
-	atlasModel := currentModel.toAtlasModel()
+	atlasModel := currentModel.toAtlasModel2()
 	startASP := false
 	atlasModel.StartASAP = &startASP
 
-	res, err := client.MaintenanceWindows.Update(context.Background(), *currentModel.ProjectId, &atlasModel)
+	_, resp, err := client.AtlasV2.MaintenanceWindowsApi.UpdateMaintenanceWindow(context.Background(), *currentModel.ProjectId, &atlasModel).Execute()
 	if err != nil {
-		_, _ = logger.Warnf("Create - error: %+v", err)
-		return progress_events.GetFailedEventByResponse(err.Error(), res.Response), nil
+		return progress_events.GetFailedEventByResponse(err.Error(), resp), nil
 	}
 
 	return handler.ProgressEvent{
@@ -117,27 +104,6 @@ func Read(req handler.Request, prevModel *Model, currentModel *Model) (handler.P
 		ResourceModel:   currentModel,
 	}
 	return event, nil
-}
-
-func get(client *mongodbatlas.Client, currentModel Model) (*mongodbatlas.MaintenanceWindow, *handler.ProgressEvent) {
-	maintenanceWindow, res, err := client.MaintenanceWindows.Get(context.Background(), *currentModel.ProjectId)
-	if err != nil {
-		_, _ = logger.Warnf("Read - error: %+v", err)
-		ev := progress_events.GetFailedEventByResponse(err.Error(), res.Response)
-		return nil, &ev
-	}
-
-	if isResponseEmpty(maintenanceWindow) {
-		_, _ = logger.Warnf("Read - resource is empty: %+v", err)
-		ev := progress_events.GetFailedEventByCode("resource not found", cloudformation.HandlerErrorCodeNotFound)
-		return nil, &ev
-	}
-
-	return maintenanceWindow, nil
-}
-
-func isResponseEmpty(maintenanceWindow *mongodbatlas.MaintenanceWindow) bool {
-	return maintenanceWindow != nil && maintenanceWindow.DayOfWeek == 0
 }
 
 func Update(req handler.Request, prevModel *Model, currentModel *Model) (handler.ProgressEvent, error) {
@@ -222,4 +188,64 @@ func Delete(req handler.Request, prevModel *Model, currentModel *Model) (handler
 
 func List(req handler.Request, prevModel *Model, currentModel *Model) (handler.ProgressEvent, error) {
 	return handler.ProgressEvent{}, errors.New("not implemented: List")
+}
+
+func (m Model) toAtlasModel() mongodbatlas.MaintenanceWindow {
+	return mongodbatlas.MaintenanceWindow{
+		DayOfWeek:            *m.DayOfWeek,
+		HourOfDay:            m.HourOfDay,
+		StartASAP:            m.StartASAP,
+		AutoDeferOnceEnabled: m.AutoDeferOnceEnabled,
+	}
+}
+
+func (m Model) toAtlasModel2() admin.GroupMaintenanceWindow {
+	return admin.GroupMaintenanceWindow{
+		DayOfWeek:            *m.DayOfWeek,
+		HourOfDay:            *m.HourOfDay,
+		StartASAP:            m.StartASAP,
+		AutoDeferOnceEnabled: m.AutoDeferOnceEnabled,
+	}
+}
+
+func get(client *mongodbatlas.Client, currentModel Model) (*mongodbatlas.MaintenanceWindow, *handler.ProgressEvent) {
+	maintenanceWindow, res, err := client.MaintenanceWindows.Get(context.Background(), *currentModel.ProjectId)
+	if err != nil {
+		_, _ = logger.Warnf("Read - error: %+v", err)
+		ev := progress_events.GetFailedEventByResponse(err.Error(), res.Response)
+		return nil, &ev
+	}
+
+	if isResponseEmpty(maintenanceWindow) {
+		_, _ = logger.Warnf("Read - resource is empty: %+v", err)
+		ev := progress_events.GetFailedEventByCode("resource not found", cloudformation.HandlerErrorCodeNotFound)
+		return nil, &ev
+	}
+
+	return maintenanceWindow, nil
+}
+
+func get2(client *util.MongoDBClient, currentModel Model) (*admin.GroupMaintenanceWindow, *handler.ProgressEvent) {
+	maintenanceWindow, resp, err := client.AtlasV2.MaintenanceWindowsApi.GetMaintenanceWindow(context.Background(), *currentModel.ProjectId).Execute()
+	if err != nil {
+		_, _ = logger.Warnf("Read - error: %+v", err)
+		ev := progress_events.GetFailedEventByResponse(err.Error(), resp)
+		return nil, &ev
+	}
+
+	if isResponseEmpty2(maintenanceWindow) {
+		_, _ = logger.Warnf("Read - resource is empty: %+v", err)
+		ev := progress_events.GetFailedEventByCode("resource not found", cloudformation.HandlerErrorCodeNotFound)
+		return nil, &ev
+	}
+
+	return maintenanceWindow, nil
+}
+
+func isResponseEmpty(maintenanceWindow *mongodbatlas.MaintenanceWindow) bool {
+	return maintenanceWindow != nil && maintenanceWindow.DayOfWeek == 0
+}
+
+func isResponseEmpty2(maintenanceWindow *admin.GroupMaintenanceWindow) bool {
+	return maintenanceWindow != nil && maintenanceWindow.DayOfWeek == 0
 }

6a0fc82e0fa69e18e0e586111a1393917d3e0ac8
Adelmar92 <69170650+Adelmar92@users.noreply.github.com>
2023-10-26 17:26:48 -0400

    fix: INTMDB-1128 fixed project apikeys update (#787)
    Co-authored-by: Agustin Bettati <bettatiagustin@gmail.com>

---
+++ cfn-resources/project/cmd/resource/resource_test.go
@@ -0,0 +1,141 @@
+// Copyright 2023 MongoDB Inc
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package resource
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/aws/aws-sdk-go/aws"
+)
+
+func TestGetChangeInAPIKeys_AddedButNotChangedOrRemoved(t *testing.T) {
+	currentKeys := []ProjectApiKey{
+		{Key: aws.String("key1"), RoleNames: []string{"role1"}},
+		{Key: aws.String("key2"), RoleNames: []string{"role2"}},
+	}
+	previousKeys := []ProjectApiKey{
+		{Key: aws.String("key1"), RoleNames: []string{"role1"}},
+	}
+
+	newKeys, changedKeys, removeKeys := getChangeInAPIKeys(currentKeys, previousKeys)
+
+	expectedNewKeys := []ProjectApiKey{
+		{Key: aws.String("key2"), RoleNames: []string{"role2"}},
+	}
+
+	if !reflect.DeepEqual(newKeys, expectedNewKeys) || len(changedKeys) > 0 || len(removeKeys) > 0 {
+		t.Errorf("Test case failed. Expected newKeys to contain all added keys.")
+	}
+}
+
+func TestGetChangeInAPIKeys_ChangedButNotAddedOrRemoved(t *testing.T) {
+	currentKeys := []ProjectApiKey{
+		{Key: aws.String("key1"), RoleNames: []string{"role1", "role1b"}},
+		{Key: aws.String("key2"), RoleNames: []string{"role2"}},
+	}
+	previousKeys := []ProjectApiKey{
+		{Key: aws.String("key1"), RoleNames: []string{"role1b", "role1"}},
+		{Key: aws.String("key2"), RoleNames: []string{"role3", "role2"}},
+	}
+
+	newKeys, changedKeys, removeKeys := getChangeInAPIKeys(currentKeys, previousKeys)
+
+	if len(newKeys) > 0 || len(removeKeys) > 0 {
+		t.Errorf("Test case failed. No new or removed keys expected.")
+	}
+
+	expectedChangedKeys := []ProjectApiKey{
+		{Key: aws.String("key2"), RoleNames: []string{"role2"}},
+	}
+
+	if !reflect.DeepEqual(changedKeys, expectedChangedKeys) {
+		t.Errorf("Test case failed. Expected changedKeys to contain keys with role changes.")
+	}
+}
+
+func TestGetChangeInAPIKeys_RemovedButNotAddedOrChanged(t *testing.T) {
+	currentKeys := []ProjectApiKey{
+		{Key: aws.String("key1"), RoleNames: []string{"role1"}},
+	}
+	previousKeys := []ProjectApiKey{
+		{Key: aws.String("key1"), RoleNames: []string{"role1"}},
+		{Key: aws.String("key2"), RoleNames: []string{"role2"}},
+	}
+
+	newKeys, changedKeys, removeKeys := getChangeInAPIKeys(currentKeys, previousKeys)
+
+	if len(newKeys) > 0 || len(changedKeys) > 0 {
+		t.Errorf("Test case failed. No new or changed keys expected.")
+	}
+
+	expectedRemoveKeys := []ProjectApiKey{
+		{Key: aws.String("key2"), RoleNames: []string{"role2"}},
+	}
+
+	if !reflect.DeepEqual(removeKeys, expectedRemoveKeys) {
+		t.Errorf("Test case failed. Expected removeKeys to contain all previous keys.")
+	}
+}
+
+func TestGetChangeInAPIKeys_AddedChangedRemovedMixed(t *testing.T) {
+	previousKeys := []ProjectApiKey{
+		{Key: aws.String("key1"), RoleNames: []string{"role1"}},
+		{Key: aws.String("key2"), RoleNames: []string{"role2"}},
+		{Key: aws.String("key5"), RoleNames: []string{"role5"}},
+		{Key: aws.String("key6"), RoleNames: []string{"role6"}},
+	}
+	currentKeys := []ProjectApiKey{
+		{Key: aws.String("key1"), RoleNames: []string{"role9"}},
+		{Key: aws.String("key2"), RoleNames: []string{"role2"}},
+		{Key: aws.String("key3"), RoleNames: []string{"role3"}},
+		{Key: aws.String("key4"), RoleNames: []string{"role4"}},
+	}
+
+	newKeys, changedKeys, removeKeys := getChangeInAPIKeys(currentKeys, previousKeys)
+
+	expectedNewKeys := []ProjectApiKey{
+		{Key: aws.String("key3"), RoleNames: []string{"role3"}},
+		{Key: aws.String("key4"), RoleNames: []string{"role4"}},
+	}
+	expectedChangedKeys := []ProjectApiKey{
+		{Key: aws.String("key1"), RoleNames: []string{"role9"}},
+	}
+	expectedRemoveKeys := []ProjectApiKey{
+		{Key: aws.String("key5"), RoleNames: []string{"role5"}},
+		{Key: aws.String("key6"), RoleNames: []string{"role6"}},
+	}
+
+	if !reflect.DeepEqual(newKeys, expectedNewKeys) || !reflect.DeepEqual(changedKeys, expectedChangedKeys) || !reflect.DeepEqual(removeKeys, expectedRemoveKeys) {
+		t.Errorf("Test case failed. Unexpected results for mixed case.")
+	}
+}
+
+func TestGetChangeInAPIKeys_NoneAddedChangedOrRemoved(t *testing.T) {
+	currentKeys := []ProjectApiKey{
+		{Key: aws.String("key1"), RoleNames: []string{"role1"}},
+		{Key: aws.String("key2"), RoleNames: []string{"role2"}},
+	}
+	previousKeys := []ProjectApiKey{
+		{Key: aws.String("key1"), RoleNames: []string{"role1"}},
+		{Key: aws.String("key2"), RoleNames: []string{"role2"}},
+	}
+
+	newKeys, changedKeys, removeKeys := getChangeInAPIKeys(currentKeys, previousKeys)
+
+	if len(newKeys) > 0 || len(changedKeys) > 0 || len(removeKeys) > 0 {
+		t.Errorf("Test case failed. No new, changed, or removed keys expected.")
+	}
+}
--- cfn-resources/util/util.go
+++ cfn-resources/util/util.go
@@ -467,3 +467,26 @@ func IntPtrToStrPtr(i *int) *string {
 	str := strconv.Itoa(*i)
 	return &str
 }
+
+func SameStringSliceWithoutOrder(x, y []string) bool {
+	if len(x) != len(y) {
+		return false
+	}
+	// create a map of string -> int
+	diff := make(map[string]int, len(x))
+	for _, _x := range x {
+		// 0 value for int is 0, so just increment a counter for the string
+		diff[_x]++
+	}
+	for _, _y := range y {
+		// If the string _y is not in diff bail out early
+		if _, ok := diff[_y]; !ok {
+			return false
+		}
+		diff[_y]--
+		if diff[_y] == 0 {
+			delete(diff, _y)
+		}
+	}
+	return len(diff) == 0
+}

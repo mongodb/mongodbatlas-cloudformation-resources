75116fd63beb01e0df4743b352dd48122fe426ec
Adelmar92 <69170650+Adelmar92@users.noreply.github.com>
2023-11-11 02:29:50 -0500

    fix: INTMDB-1158 search index analyzers (#801)
    Co-authored-by: Leo Antoli <430982+lantoli@users.noreply.github.com>

---
+++ cfn-resources/search-index/cmd/resource/resource_test.go
@@ -0,0 +1,111 @@
+// Copyright 2023 MongoDB Inc
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package resource
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/aws/smithy-go/ptr"
+	"go.mongodb.org/atlas-sdk/v20231001001/admin"
+)
+
+func TestConvertToAnySliceEmptyList(t *testing.T) {
+	var input []string
+	var expected []interface{}
+
+	result, err := convertToAnySlice(input)
+
+	if err != nil {
+		t.Errorf("Unexpected error: %v", err)
+	}
+
+	if !reflect.DeepEqual(result, expected) {
+		t.Errorf("Result does not match the expected value. Got: %v, Expected: %v", result, expected)
+	}
+}
+
+func TestConvertToAnySliceValidJSON(t *testing.T) {
+	input := []string{
+		`{"type": "icuNormalize"}`,
+	}
+	expected := []interface{}{
+		map[string]interface{}{"type": "icuNormalize"},
+	}
+
+	result, err := convertToAnySlice(input)
+
+	if err != nil {
+		t.Errorf("Unexpected error: %v", err)
+	}
+
+	if !reflect.DeepEqual(result, expected) {
+		t.Errorf("Result does not match the expected value. Got: %v, Expected: %v", result, expected)
+	}
+}
+
+func TestConvertToAnySliceInvalidJSON(t *testing.T) {
+	input := []string{
+		`this is an invalid json`,
+	}
+	var expected []interface{}
+
+	result, err := convertToAnySlice(input)
+
+	if err == nil {
+		t.Errorf("Expected an error, but got nil")
+	}
+
+	if !reflect.DeepEqual(result, expected) {
+		t.Errorf("Result does not match the expected value. Got: %v, Expected: %v", result, expected)
+	}
+}
+
+func TestNewTokenizerModelWithNilInput(t *testing.T) {
+	var tokenizer *ApiAtlasFTSAnalyzersTokenizer
+
+	result := newTokenizerModel(tokenizer)
+
+	expected := admin.ApiAtlasFTSAnalyzersTokenizer{}
+	if !reflect.DeepEqual(result, expected) {
+		t.Errorf("Expected: %v, but got: %v", expected, result)
+	}
+}
+
+func TestNewTokenizerModelWithPartialParameters(t *testing.T) {
+	tokenizer := &ApiAtlasFTSAnalyzersTokenizer{
+		MaxGram:        nil,
+		MinGram:        nil,
+		Type:           ptr.String("standard"),
+		Group:          ptr.Int(1),
+		Pattern:        nil,
+		MaxTokenLength: ptr.Int(10),
+	}
+
+	result := newTokenizerModel(tokenizer)
+
+	expected := admin.ApiAtlasFTSAnalyzersTokenizer{
+		MaxGram:        nil,
+		MinGram:        nil,
+		Type:           ptr.String("standard"),
+		Group:          ptr.Int(1),
+		Pattern:        nil,
+		MaxTokenLength: ptr.Int(10),
+	}
+
+	if !reflect.DeepEqual(result, expected) {
+		t.Errorf("Expected: %v, but got: %v", expected, result)
+	}
+}

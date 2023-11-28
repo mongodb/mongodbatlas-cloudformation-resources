d0bacad9e9b4bd3fec6fc7f98c04a0278305b45d
github-merge-queue <github-merge-queue@users.noreply.github.com>
2023-11-28 08:33:39 +0000

    build(deps): bump go.mongodb.org/atlas-sdk

--- cfn-resources/util/util.go
+++ cfn-resources/util/util.go
@@ -37,7 +37,7 @@ import (
 	"github.com/mongodb/mongodbatlas-cloudformation-resources/profile"
 	"github.com/mongodb/mongodbatlas-cloudformation-resources/util/logger"
 	"github.com/mongodb/mongodbatlas-cloudformation-resources/version"
-	atlasSDK "go.mongodb.org/atlas-sdk/v20231115001/admin"
+	atlasSDK "go.mongodb.org/atlas-sdk/v20231115002/admin"
 	"go.mongodb.org/atlas/mongodbatlas"
 	realmAuth "go.mongodb.org/realm/auth"
 	"go.mongodb.org/realm/realm"

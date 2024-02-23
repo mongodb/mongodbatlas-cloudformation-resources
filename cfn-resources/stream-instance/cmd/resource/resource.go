f2a2cfa32b358716944645fa84b3c138fed14675
Oriol Arbusi <oriol.abadal@mongodb.com>
2024-02-23 11:27:42 +0100

    finish read

--- cfn-resources/stream-instance/cmd/resource/resource.go
+++ cfn-resources/stream-instance/cmd/resource/resource.go
@@ -3,8 +3,10 @@ package resource
 import (
 	"context"
 	"errors"
+	"net/http"
 
 	"github.com/aws-cloudformation/cloudformation-cli-go-plugin/cfn/handler"
+	"github.com/aws/aws-sdk-go/service/cloudformation"
 
 	"github.com/mongodb/mongodbatlas-cloudformation-resources/util"
 	"github.com/mongodb/mongodbatlas-cloudformation-resources/util/constants"
@@ -55,8 +57,41 @@ func Create(req handler.Request, prevModel *Model, currentModel *Model) (handler
 }
 
 func Read(req handler.Request, prevModel *Model, currentModel *Model) (handler.ProgressEvent, error) {
+	setup()
+	util.SetDefaultProfileIfNotDefined(&currentModel.Profile)
+	if errEvent := validator.ValidateModel(CreateRequiredFields, currentModel); errEvent != nil {
+		return *errEvent, nil
+	}
 
-	return handler.ProgressEvent{}, errors.New("Not implemented: Read")
+	client, handlerError := util.NewAtlasClient(&req, currentModel.Profile)
+	if handlerError != nil {
+		_, _ = logger.Warnf("CreateMongoDBClient error: %v", handlerError)
+		return *handlerError, errors.New(handlerError.Message)
+	}
+
+	atlasV2 := client.AtlasSDK
+
+	streamInstance, resp, err := atlasV2.StreamsApi.GetStreamInstance(context.Background(), *currentModel.GroupId, *currentModel.Name).Execute()
+	if err != nil {
+		if resp != nil && resp.StatusCode == http.StatusNotFound {
+			return handler.ProgressEvent{
+				Message:          err.Error(),
+				OperationStatus:  handler.Failed,
+				HandlerErrorCode: cloudformation.HandlerErrorCodeNotFound}, nil
+		}
+	}
+
+	currentModel.Id = streamInstance.Id
+	currentModel.Hostnames = streamInstance.GetHostnames()
+	currentModel.DataProcessRegion = newModelDataRegion(streamInstance.DataProcessRegion)
+	currentModel.StreamConfig = newModelStreamConfig(streamInstance.StreamConfig)
+	currentModel.Connections = newModelConnections(streamInstance.GetConnections())
+
+	return handler.ProgressEvent{
+		OperationStatus: cloudformation.OperationStatusSuccess,
+		Message:         "Read Complete",
+		ResourceModel:   currentModel,
+	}, nil
 }
 
 func Update(req handler.Request, prevModel *Model, currentModel *Model) (handler.ProgressEvent, error) {
@@ -86,4 +121,63 @@ func newStreamsTenant(model *Model) *admin.StreamsTenant {
 			Tier: model.StreamConfig.Tier,
 		},
 	}
+}
+
+func newModelDataRegion(dataProcessRegion *admin.StreamsDataProcessRegion) *StreamsDataProcessRegion {
+	return &StreamsDataProcessRegion{
+		CloudProvider: &dataProcessRegion.CloudProvider,
+		Region:        &dataProcessRegion.Region,
+	}
+}
+
+func newModelStreamConfig(streamConfig *admin.StreamConfig) *StreamConfig {
+	return &StreamConfig{
+		Tier: streamConfig.Tier,
+	}
+}
+
+func newModelDbRoleToExecute(dbRole *admin.DBRoleToExecute) *DBRoleToExecute {
+	return &DBRoleToExecute{
+		Role: dbRole.Role,
+		Type: dbRole.Type,
+	}
+}
+
+func newModelAuthentication(authentication *admin.StreamsKafkaAuthentication) *StreamsKafkaAuthentication {
+	return &StreamsKafkaAuthentication{
+		Mechanism: authentication.Mechanism,
+		Password:  authentication.Password,
+		Username:  authentication.Username,
+	}
+}
+
+func newModelSecurity(security *admin.StreamsKafkaSecurity) *StreamsKafkaSecurity {
+	return &StreamsKafkaSecurity{
+		BrokerPublicCertificate: security.BrokerPublicCertificate,
+		Protocol:                security.Protocol,
+	}
+}
+
+func newModelConnections(streamConfig []admin.StreamsConnection) []StreamsConnection {
+	if len(streamConfig) == 0 {
+		return nil
+	}
+
+	connections := make([]StreamsConnection, 0)
+	for _, connection := range streamConfig {
+		modelConnection := StreamsConnection{
+			Name: connection.Name,
+			Type: connection.Type,
+		}
+		if connection.GetType() == Cluster {
+			modelConnection.ClusterName = connection.ClusterName
+			modelConnection.DbRoleToExecute = newModelDbRoleToExecute(connection.DbRoleToExecute)
+		} else if connection.GetType() == Kafka {
+			modelConnection.BootstrapServers = connection.BootstrapServers
+			modelConnection.Authentication = newModelAuthentication(connection.Authentication)
+			modelConnection.Security = newModelSecurity(connection.Security)
+		}
+		connections = append(connections, modelConnection)
+	}
+	return connections
 }

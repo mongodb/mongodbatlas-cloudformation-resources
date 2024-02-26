f667623223b4f7e091eb46a77d91be62babd989a
Aastha Mahendru <aastha.mahendru@mongodb.com>
2024-02-26 08:13:21 +0000

    initial schema and directory

---
+++ cfn-resources/stream-connection/cmd/resource/resource.go
@@ -0,0 +1,257 @@
+package resource
+
+import (
+	"github.com/aws-cloudformation/cloudformation-cli-go-plugin/cfn/handler"
+	"github.com/aws/aws-sdk-go/service/cloudformation"
+	"github.com/mongodb/mongodbatlas-cloudformation-resources/util"
+	log "github.com/mongodb/mongodbatlas-cloudformation-resources/util/logger"
+	progress_events "github.com/mongodb/mongodbatlas-cloudformation-resources/util/progressevent"
+	"github.com/mongodb/mongodbatlas-cloudformation-resources/util/validator"
+	mongodbatlas "go.mongodb.org/atlas/mongodbatlas"
+)
+
+var CreateRequiredFields = []string{"ApiKeys.PublicKey", "ApiKeys.PrivateKey"}
+var ReadRequiredFields = []string{"GroupId", "TenantName", "ConnectionName", "ApiKeys.PublicKey", "ApiKeys.PrivateKey"}
+var UpdateRequiredFields = []string{"ApiKeys.PublicKey", "ApiKeys.PrivateKey"}
+var DeleteRequiredFields = []string{"GroupId", "TenantName", "ConnectionName", "ApiKeys.PublicKey", "ApiKeys.PrivateKey"}
+var ListRequiredFields = []string{"ApiKeys.PublicKey", "ApiKeys.PrivateKey"}
+
+func validateModel(fields []string, model *Model) *handler.ProgressEvent {
+	return validator.ValidateModel(fields, model)
+}
+
+func setup() {
+	util.SetupLogger("mongodb-atlas-streamconnection")
+}
+
+func Create(req handler.Request, prevModel *Model, currentModel *Model) (handler.ProgressEvent, error) {
+	setup()
+
+	log.Debugf("Create() currentModel:%+v", currentModel)
+
+	// Validation
+	modelValidation := validateModel(CreateRequiredFields, currentModel)
+	if modelValidation != nil {
+		return *modelValidation, nil
+	}
+
+	// Create atlas client
+	client, err := util.CreateMongoDBClient(*currentModel.ApiKeys.PublicKey, *currentModel.ApiKeys.PrivateKey)
+	if err != nil {
+		log.Debugf("Create - error: %+v", err)
+		return handler.ProgressEvent{
+			HandlerErrorCode: cloudformation.HandlerErrorCodeInvalidRequest,
+			Message:          err.Error(),
+			OperationStatus:  handler.Failed,
+		}, nil
+	}
+	var res *mongodbatlas.Response
+
+	//
+	/*
+	    // Pseudocode:
+	    res , resModel, err := client.Streamconnection.Create(context.Background(),&mongodbatlas.Streamconnection{
+	   })
+
+	*/
+
+	if err != nil {
+		log.Debugf("Create - error: %+v", err)
+		return progress_events.GetFailedEventByResponse(err.Error(), res.Response), nil
+	}
+	log.Debugf("Atlas Client %v", client)
+
+	// Response
+	return handler.ProgressEvent{
+		OperationStatus: handler.Success,
+		ResourceModel:   currentModel,
+	}, nil
+}
+
+func Read(req handler.Request, prevModel *Model, currentModel *Model) (handler.ProgressEvent, error) {
+	setup()
+
+	log.Debugf("Read() currentModel:%+v", currentModel)
+
+	// Validation
+	modelValidation := validateModel(ReadRequiredFields, currentModel)
+	if modelValidation != nil {
+		return *modelValidation, nil
+	}
+
+	// Create atlas client
+	client, err := util.CreateMongoDBClient(*currentModel.ApiKeys.PublicKey, *currentModel.ApiKeys.PrivateKey)
+	if err != nil {
+		log.Debugf("Read - error: %+v", err)
+		return handler.ProgressEvent{
+			HandlerErrorCode: cloudformation.HandlerErrorCodeInvalidRequest,
+			Message:          err.Error(),
+			OperationStatus:  handler.Failed,
+		}, nil
+	}
+	var res *mongodbatlas.Response
+
+	/*
+	   Considerable params from currentModel:
+	   GroupId, TenantName, ConnectionName, ...
+	*/
+	/*
+	    // Pseudocode:
+	    res , resModel, err := client.Streamconnection.Read(context.Background(),&mongodbatlas.Streamconnection{
+	   	GroupId:currentModel.GroupId,
+	   	TenantName:currentModel.TenantName,
+	   	ConnectionName:currentModel.ConnectionName,
+	   })
+
+	*/
+
+	if err != nil {
+		log.Debugf("Read - error: %+v", err)
+		return progress_events.GetFailedEventByResponse(err.Error(), res.Response), nil
+	}
+	log.Debugf("Atlas Client %v", client)
+
+	// Response
+	return handler.ProgressEvent{
+		OperationStatus: handler.Success,
+		ResourceModel:   currentModel,
+	}, nil
+}
+
+func Update(req handler.Request, prevModel *Model, currentModel *Model) (handler.ProgressEvent, error) {
+	setup()
+
+	log.Debugf("Update() currentModel:%+v", currentModel)
+
+	// Validation
+	modelValidation := validateModel(UpdateRequiredFields, currentModel)
+	if modelValidation != nil {
+		return *modelValidation, nil
+	}
+
+	// Create atlas client
+	client, err := util.CreateMongoDBClient(*currentModel.ApiKeys.PublicKey, *currentModel.ApiKeys.PrivateKey)
+	if err != nil {
+		log.Debugf("Update - error: %+v", err)
+		return handler.ProgressEvent{
+			HandlerErrorCode: cloudformation.HandlerErrorCodeInvalidRequest,
+			Message:          err.Error(),
+			OperationStatus:  handler.Failed,
+		}, nil
+	}
+	var res *mongodbatlas.Response
+
+	//
+	/*
+	    // Pseudocode:
+	    res , resModel, err := client.Streamconnection.Update(context.Background(),&mongodbatlas.Streamconnection{
+	   })
+
+	*/
+
+	if err != nil {
+		log.Debugf("Update - error: %+v", err)
+		return progress_events.GetFailedEventByResponse(err.Error(), res.Response), nil
+	}
+	log.Debugf("Atlas Client %v", client)
+
+	// Response
+	return handler.ProgressEvent{
+		OperationStatus: handler.Success,
+		ResourceModel:   currentModel,
+	}, nil
+}
+
+func Delete(req handler.Request, prevModel *Model, currentModel *Model) (handler.ProgressEvent, error) {
+	setup()
+
+	log.Debugf("Delete() currentModel:%+v", currentModel)
+
+	// Validation
+	modelValidation := validateModel(DeleteRequiredFields, currentModel)
+	if modelValidation != nil {
+		return *modelValidation, nil
+	}
+
+	// Create atlas client
+	client, err := util.CreateMongoDBClient(*currentModel.ApiKeys.PublicKey, *currentModel.ApiKeys.PrivateKey)
+	if err != nil {
+		log.Debugf("Delete - error: %+v", err)
+		return handler.ProgressEvent{
+			HandlerErrorCode: cloudformation.HandlerErrorCodeInvalidRequest,
+			Message:          err.Error(),
+			OperationStatus:  handler.Failed,
+		}, nil
+	}
+	var res *mongodbatlas.Response
+
+	/*
+	   Considerable params from currentModel:
+	   GroupId, TenantName, ConnectionName, ...
+	*/
+	/*
+	    // Pseudocode:
+	    res , resModel, err := client.Streamconnection.Delete(context.Background(),&mongodbatlas.Streamconnection{
+	   	GroupId:currentModel.GroupId,
+	   	TenantName:currentModel.TenantName,
+	   	ConnectionName:currentModel.ConnectionName,
+	   })
+
+	*/
+
+	if err != nil {
+		log.Debugf("Delete - error: %+v", err)
+		return progress_events.GetFailedEventByResponse(err.Error(), res.Response), nil
+	}
+	log.Debugf("Atlas Client %v", client)
+
+	// Response
+	return handler.ProgressEvent{
+		OperationStatus: handler.Success,
+		ResourceModel:   currentModel,
+	}, nil
+}
+
+func List(req handler.Request, prevModel *Model, currentModel *Model) (handler.ProgressEvent, error) {
+	setup()
+
+	log.Debugf("List() currentModel:%+v", currentModel)
+
+	// Validation
+	modelValidation := validateModel(ListRequiredFields, currentModel)
+	if modelValidation != nil {
+		return *modelValidation, nil
+	}
+
+	// Create atlas client
+	client, err := util.CreateMongoDBClient(*currentModel.ApiKeys.PublicKey, *currentModel.ApiKeys.PrivateKey)
+	if err != nil {
+		log.Debugf("List - error: %+v", err)
+		return handler.ProgressEvent{
+			HandlerErrorCode: cloudformation.HandlerErrorCodeInvalidRequest,
+			Message:          err.Error(),
+			OperationStatus:  handler.Failed,
+		}, nil
+	}
+	var res *mongodbatlas.Response
+
+	//
+	/*
+	    // Pseudocode:
+	    res , resModel, err := client.Streamconnection.List(context.Background(),&mongodbatlas.Streamconnection{
+	   })
+
+	*/
+
+	if err != nil {
+		log.Debugf("List - error: %+v", err)
+		return progress_events.GetFailedEventByResponse(err.Error(), res.Response), nil
+	}
+	log.Debugf("Atlas Client %v", client)
+
+	// Response
+	return handler.ProgressEvent{
+		OperationStatus: handler.Success,
+		ResourceModel:   currentModel,
+	}, nil
+}

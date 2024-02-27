4dd2f11ac449c7c95ecf27dd17ad34918cb81cd4
Oriol Arbusi <oriol.abadal@mongodb.com>
2024-02-27 16:53:38 +0100

    change from any rto interface

--- cfn-resources/stream-instance/cmd/resource/resource.go
+++ cfn-resources/stream-instance/cmd/resource/resource.go
@@ -190,7 +190,7 @@ func List(req handler.Request, prevModel *Model, currentModel *Model) (handler.P
 		ok = streamInstances.GetTotalCount() > len(accumulatedStreamInstances)
 	}
 
-	response := make([]any, len(accumulatedStreamInstances))
+	response := make([]interface{}, len(accumulatedStreamInstances))
 	if len(accumulatedStreamInstances) > 0 {
 		for i := range accumulatedStreamInstances {
 			response = append(response, accumulatedStreamInstances[i])

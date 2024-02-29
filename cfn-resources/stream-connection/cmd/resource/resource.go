d1d1b3f74e3b9b8510fe3539150e6d85a1b11d6a
Aastha Mahendru <aastha.mahendru@mongodb.com>
2024-02-29 22:23:58 +0000

    lint fix

--- cfn-resources/stream-connection/cmd/resource/resource.go
+++ cfn-resources/stream-connection/cmd/resource/resource.go
@@ -1,3 +1,14 @@
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package resource
 
 import (
@@ -55,7 +66,6 @@ func setup(cfnFunc constants.CfnFunctions, req handler.Request, currentModel *Mo
 		return nil, peErr
 	}
 	return client.AtlasSDK, nil
-
 }
 
 func Create(req handler.Request, prevModel *Model, currentModel *Model) (handler.ProgressEvent, error) {

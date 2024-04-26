e3637d0639c3d7a977d14682b0e489aa331a0945
EspenAlbert <espen.albert@mongodb.com>
2024-04-26 11:14:54 +0100

    chore: fix publish script

--- cfn-resources/project/test/cfn-test-create-inputs.sh
+++ cfn-resources/project/test/cfn-test-create-inputs.sh
@@ -42,7 +42,7 @@ api_key_id=$(atlas organizations apikeys create --desc "cfn-boto-key-${CFN_TEST_
 team_name="cfn-boto-team-${CFN_TEST_TAG}"
 user_name=$(atlas organizations users list --output json | jq -r '.results[0].emailAddress')
 echo "${user_name}"
-team_id=$(atlas teams describe --name "$team_name" --output json | jq -r ".id")
+team_id=$(atlas teams describe --name "$team_name" --output json | jq -r ".id") || echo ""
 if [ -z "$team_id" ]; then
 	team_id=$(atlas team create "${team_name}" --username "${user_name}" --orgId "${org_id}" --output json | jq -r '.id')
 fi
@@ -74,4 +74,3 @@ jq --arg org "${org_id}" \
 	"test/inputs_2_update.template.json" >"inputs/inputs_2_update.json"
 
 ls -l inputs
-echo "TODO: Delete the team and api_key created above"

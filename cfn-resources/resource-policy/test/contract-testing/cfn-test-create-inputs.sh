b8423225670c8f4d1733361c61c049a928a69b6c
Oriol <oriol.abadal@mongodb.com>
2024-09-18 10:14:59 +0200

    chore: Removes unnecessary checks in input generation (#1163)

--- cfn-resources/resource-policy/test/contract-testing/cfn-test-create-inputs.sh
+++ cfn-resources/resource-policy/test/contract-testing/cfn-test-create-inputs.sh
@@ -9,11 +9,6 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-if [ -z "${AWS_DEFAULT_REGION+x}" ]; then
-	echo "AWS_DEFAULT_REGION must be set"
-	exit 1
-fi
-
 # setting projectName
 policyName="ct-resource-policy-$((1 + RANDOM % 10000))"
 

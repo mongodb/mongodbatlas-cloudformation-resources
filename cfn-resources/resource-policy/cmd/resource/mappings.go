99ff21109cf7f3242f683563660908f9742fc51b
github-merge-queue <118344674+github-merge-queue@users.noreply.github.com>
2024-12-31 08:36:37 +0000

    build(deps): bump go.mongodb.org/atlas-sdk

--- cfn-resources/resource-policy/cmd/resource/mappings.go
+++ cfn-resources/resource-policy/cmd/resource/mappings.go
@@ -16,7 +16,7 @@ package resource
 
 import (
 	"github.com/mongodb/mongodbatlas-cloudformation-resources/util"
-	"go.mongodb.org/atlas-sdk/v20241113003/admin"
+	"go.mongodb.org/atlas-sdk/v20241113004/admin"
 )
 
 func NewResourcePolicyCreateReq(model *Model) *admin.ApiAtlasResourcePolicyCreate {

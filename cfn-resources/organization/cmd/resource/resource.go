336187936216af3e5660557b2adb9fc332c90cb6
github-merge-queue <118344674+github-merge-queue@users.noreply.github.com>
2025-04-01 08:39:16 +0000

    build(deps): bump go.mongodb.org/atlas-sdk

--- cfn-resources/organization/cmd/resource/resource.go
+++ cfn-resources/organization/cmd/resource/resource.go
@@ -21,7 +21,7 @@ import (
 	"net/http"
 	"time"
 
-	"go.mongodb.org/atlas-sdk/v20241113004/admin"
+	"go.mongodb.org/atlas-sdk/v20250312001/admin"
 
 	"github.com/aws-cloudformation/cloudformation-cli-go-plugin/cfn/handler"
 	"github.com/aws/aws-sdk-go/service/cloudformation"

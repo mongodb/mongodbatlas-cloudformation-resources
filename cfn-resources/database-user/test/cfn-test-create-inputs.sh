ccee0f570304184f774a39115215eb58d634089b
Espen Albert <espen.albert@mongodb.com>
2025-04-15 10:34:04 +0100

    test: Update test inputs

--- cfn-resources/database-user/test/cfn-test-create-inputs.sh
+++ cfn-resources/database-user/test/cfn-test-create-inputs.sh
@@ -18,6 +18,7 @@ rm -rf inputs
 mkdir inputs
 
 projectName="${1}"
+MONGODB_ATLAS_PROFILE=${MONGODB_ATLAS_PROFILE:-"default"}
 projectId=$(atlas projects list --output json | jq --arg NAME "${projectName}" -r '.results[] | select(.name==$NAME) | .id')
 if [ -z "$projectId" ]; then
 	projectId=$(atlas projects create "${projectName}" --output=json | jq -r '.id')
@@ -32,8 +33,8 @@ echo "Check if a project is created $projectId"
 cd "$(dirname "$0")" || exit
 for inputFile in inputs_*; do
 	outputFile=${inputFile//$WORDTOREMOVE/}
-	jq --arg ProjectId "$projectId" \
-		'.ProjectId?|=$ProjectId' \
+	jq --arg ProjectId "$projectId" --arg Profile "${MONGODB_ATLAS_PROFILE}" \
+		'.ProjectId?|=$ProjectId | .Profile?|=$Profile' \
 		"$inputFile" >"../inputs/$outputFile"
 done
 cd ..

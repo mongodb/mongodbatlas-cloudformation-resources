164b3b5c5dcc5be8bb0829dd3db4388b0c348687
sivaram-mongodb <sivaram.sj@mongodb.com>
2026-01-21 18:50:43 +0530

    feat: add Stream Processor resource (#1532)
    Co-authored-by: sivaram-mongodb <sivaram@mongodb.com>
    Co-authored-by: ParthasarathyV <parthasarathy.varadhan@mongodb.com>
    Co-authored-by: Rakhul S Prakash <rakhul.s.prakash@peerislands.io>
    Co-authored-by: ParthasarathyV <114770988+ParthasarathyV@users.noreply.github.com>

---
+++ cfn-resources/stream-processor/cmd/resource/handlers.go
@@ -0,0 +1,338 @@
+// Copyright 2026 MongoDB Inc
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package resource
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"go.mongodb.org/atlas-sdk/v20250312012/admin"
+
+	"github.com/aws-cloudformation/cloudformation-cli-go-plugin/cfn/handler"
+
+	"github.com/mongodb/mongodbatlas-cloudformation-resources/util"
+	"github.com/mongodb/mongodbatlas-cloudformation-resources/util/constants"
+)
+
+func IsCallback(req *handler.Request) bool {
+	_, found := req.CallbackContext["callbackStreamProcessor"]
+	return found
+}
+
+func HandleCreate(req *handler.Request, client *util.MongoDBClient, model *Model) handler.ProgressEvent {
+	if IsCallback(req) {
+		callbackCtx := getCallbackData(*req)
+		if peErr := validateCallbackData(callbackCtx); peErr != nil {
+			return *peErr
+		}
+		return handleCreateCallback(
+			context.Background(),
+			client,
+			model,
+			callbackCtx,
+		)
+	}
+
+	workspaceName := util.SafeString(model.WorkspaceName)
+
+	ctx := context.Background()
+	projectID := util.SafeString(model.ProjectId)
+	processorName := util.SafeString(model.ProcessorName)
+
+	var needsStarting bool
+	if model.DesiredState != nil {
+		state := *model.DesiredState
+		switch state {
+		case StartedState:
+			needsStarting = true
+		case CreatedState:
+			needsStarting = false
+		default:
+			return handler.ProgressEvent{
+				OperationStatus: handler.Failed,
+				Message:         "When creating a stream processor, the only valid states are CREATED and STARTED",
+			}
+		}
+	}
+
+	streamProcessorReq, err := NewStreamProcessorReq(model)
+	if err != nil {
+		return handler.ProgressEvent{
+			OperationStatus: handler.Failed,
+			Message:         fmt.Sprintf("Error creating stream processor request: %s", err.Error()),
+		}
+	}
+
+	_, apiResp, err := client.AtlasSDK.StreamsApi.CreateStreamProcessor(ctx, projectID, workspaceName, streamProcessorReq).Execute()
+	if err != nil {
+		return handleError(apiResp, constants.CREATE, err)
+	}
+
+	timeoutStr := ""
+	if model.Timeouts != nil && model.Timeouts.Create != nil {
+		timeoutStr = *model.Timeouts.Create
+	}
+
+	deleteOnCreateTimeout := true
+	if model.DeleteOnCreateTimeout != nil {
+		deleteOnCreateTimeout = *model.DeleteOnCreateTimeout
+	}
+
+	inProgressModel := &Model{}
+	if model != nil {
+		*inProgressModel = *model
+		inProgressModel.DeleteOnCreateTimeout = nil
+	}
+	copyIdentifyingFields(inProgressModel, model)
+
+	return handler.ProgressEvent{
+		OperationStatus:      handler.InProgress,
+		Message:              constants.Pending,
+		ResourceModel:        inProgressModel,
+		CallbackDelaySeconds: defaultCallbackDelaySeconds,
+		CallbackContext: buildCallbackContext(projectID, workspaceName, processorName, map[string]any{
+			"needsStarting":         needsStarting,
+			"startTime":             time.Now().Format(time.RFC3339),
+			"timeoutDuration":       timeoutStr,
+			"deleteOnCreateTimeout": deleteOnCreateTimeout,
+		}),
+	}
+}
+
+func HandleRead(req *handler.Request, client *util.MongoDBClient, model *Model) handler.ProgressEvent {
+	workspaceName := util.SafeString(model.WorkspaceName)
+	projectID := util.SafeString(model.ProjectId)
+	processorName := util.SafeString(model.ProcessorName)
+
+	streamProcessor, apiResp, err := client.AtlasSDK.StreamsApi.GetStreamProcessorWithParams(context.Background(),
+		&admin.GetStreamProcessorApiParams{
+			GroupId:       projectID,
+			TenantName:    workspaceName,
+			ProcessorName: processorName,
+		}).Execute()
+	if err != nil {
+		if util.StatusNotFound(apiResp) {
+			return handler.ProgressEvent{
+				OperationStatus:  handler.Failed,
+				Message:          "Resource not found",
+				HandlerErrorCode: "NotFound",
+			}
+		}
+		return handleError(apiResp, constants.READ, err)
+	}
+
+	resourceModel, err := GetStreamProcessorModel(streamProcessor, model)
+	if err != nil {
+		return handler.ProgressEvent{
+			OperationStatus: handler.Failed,
+			Message:         fmt.Sprintf("Error converting stream processor model: %s", err.Error()),
+		}
+	}
+
+	copyIdentifyingFields(resourceModel, model)
+
+	return handler.ProgressEvent{
+		OperationStatus: handler.Success,
+		Message:         constants.ReadComplete,
+		ResourceModel:   resourceModel,
+	}
+}
+
+func HandleUpdate(req *handler.Request, client *util.MongoDBClient, prevModel *Model, model *Model) handler.ProgressEvent {
+	if IsCallback(req) {
+		callbackCtx := getCallbackData(*req)
+		if peErr := validateCallbackData(callbackCtx); peErr != nil {
+			return *peErr
+		}
+		return handleUpdateCallback(
+			context.Background(),
+			client,
+			model,
+			callbackCtx,
+		)
+	}
+
+	workspaceName := util.SafeString(model.WorkspaceName)
+
+	ctx := context.Background()
+	projectID := util.SafeString(model.ProjectId)
+	processorName := util.SafeString(model.ProcessorName)
+
+	requestParams := &admin.GetStreamProcessorApiParams{
+		GroupId:       projectID,
+		TenantName:    workspaceName,
+		ProcessorName: processorName,
+	}
+
+	currentStreamProcessor, apiResp, err := client.AtlasSDK.StreamsApi.GetStreamProcessorWithParams(ctx, requestParams).Execute()
+	if err != nil {
+		if util.StatusNotFound(apiResp) {
+			return handler.ProgressEvent{
+				OperationStatus:  handler.Failed,
+				Message:          "Resource not found",
+				HandlerErrorCode: "NotFound",
+			}
+		}
+		return handleError(apiResp, constants.READ, err)
+	}
+
+	currentState := currentStreamProcessor.GetState()
+
+	desiredState := currentState
+	if model.DesiredState != nil && *model.DesiredState != "" {
+		desiredState = *model.DesiredState
+	} else if prevModel != nil && prevModel.DesiredState != nil && *prevModel.DesiredState != "" {
+		desiredState = *prevModel.DesiredState
+	}
+
+	if errMsg, isValid := validateUpdateStateTransition(currentState, desiredState); !isValid {
+		return handler.ProgressEvent{
+			OperationStatus: handler.Failed,
+			Message:         errMsg,
+		}
+	}
+
+	if currentState == StartedState {
+		_, err := client.AtlasSDK.StreamsApi.StopStreamProcessorWithParams(ctx,
+			&admin.StopStreamProcessorApiParams{
+				GroupId:       projectID,
+				TenantName:    workspaceName,
+				ProcessorName: processorName,
+			},
+		).Execute()
+		if err != nil {
+			return handler.ProgressEvent{
+				OperationStatus: handler.Failed,
+				Message:         fmt.Sprintf("Error stopping stream processor: %s", err.Error()),
+			}
+		}
+
+		inProgressModel := &Model{}
+		if model != nil {
+			*inProgressModel = *model
+			inProgressModel.DeleteOnCreateTimeout = nil
+		}
+		copyIdentifyingFields(inProgressModel, model)
+
+		return handler.ProgressEvent{
+			OperationStatus:      handler.InProgress,
+			Message:              constants.Pending,
+			ResourceModel:        inProgressModel,
+			CallbackDelaySeconds: defaultCallbackDelaySeconds,
+			CallbackContext: buildCallbackContext(projectID, workspaceName, processorName, map[string]any{
+				"desiredState": desiredState,
+			}),
+		}
+	}
+
+	modifyAPIRequestParams, err := NewStreamProcessorUpdateReq(model)
+	if err != nil {
+		return handler.ProgressEvent{
+			OperationStatus: handler.Failed,
+			Message:         fmt.Sprintf("Error creating update request: %s", err.Error()),
+		}
+	}
+
+	streamProcessorResp, apiResp, err := client.AtlasSDK.StreamsApi.UpdateStreamProcessorWithParams(ctx, modifyAPIRequestParams).Execute()
+	if err != nil {
+		return handleError(apiResp, constants.UPDATE, err)
+	}
+
+	if desiredState == StartedState {
+		if peErr := startStreamProcessor(ctx, client.AtlasSDK, projectID, workspaceName, processorName); peErr != nil {
+			return *peErr
+		}
+
+		inProgressModel := &Model{}
+		if model != nil {
+			*inProgressModel = *model
+			inProgressModel.DeleteOnCreateTimeout = nil
+		}
+		copyIdentifyingFields(inProgressModel, model)
+
+		return handler.ProgressEvent{
+			OperationStatus:      handler.InProgress,
+			Message:              constants.Pending,
+			ResourceModel:        inProgressModel,
+			CallbackDelaySeconds: defaultCallbackDelaySeconds,
+			CallbackContext: buildCallbackContext(projectID, workspaceName, processorName, map[string]any{
+				"desiredState": desiredState,
+			}),
+		}
+	}
+
+	return finalizeModel(streamProcessorResp, model, constants.Complete)
+}
+
+func HandleDelete(req *handler.Request, client *util.MongoDBClient, model *Model) handler.ProgressEvent {
+	workspaceName := util.SafeString(model.WorkspaceName)
+
+	ctx := context.Background()
+	projectID := util.SafeString(model.ProjectId)
+	processorName := util.SafeString(model.ProcessorName)
+
+	apiResp, err := client.AtlasSDK.StreamsApi.DeleteStreamProcessor(ctx, projectID, workspaceName, processorName).Execute()
+	if err != nil {
+		if util.StatusNotFound(apiResp) {
+			return handler.ProgressEvent{
+				OperationStatus:  handler.Failed,
+				Message:          "Resource not found",
+				HandlerErrorCode: "NotFound",
+			}
+		}
+		return handler.ProgressEvent{
+			OperationStatus: handler.Failed,
+			Message:         fmt.Sprintf("Error deleting stream processor: %s", err.Error()),
+		}
+	}
+
+	return handler.ProgressEvent{
+		OperationStatus: handler.Success,
+		Message:         constants.Complete,
+	}
+}
+
+func HandleList(req *handler.Request, client *util.MongoDBClient, model *Model) handler.ProgressEvent {
+	workspaceName := util.SafeString(model.WorkspaceName)
+
+	ctx := context.Background()
+	projectID := util.SafeString(model.ProjectId)
+
+	accumulatedProcessors, apiResp, err := getAllStreamProcessors(ctx, client.AtlasSDK, projectID, workspaceName)
+	if err != nil {
+		return handleError(apiResp, constants.LIST, err)
+	}
+
+	response := make([]interface{}, 0, len(accumulatedProcessors))
+	for i := range accumulatedProcessors {
+		modelItem, err := GetStreamProcessorModel(&accumulatedProcessors[i], model)
+		if err != nil {
+			return handler.ProgressEvent{
+				OperationStatus: handler.Failed,
+				Message:         fmt.Sprintf("Error converting stream processor model: %s", err.Error()),
+			}
+		}
+
+		copyIdentifyingFields(modelItem, model)
+		response = append(response, modelItem)
+	}
+
+	return handler.ProgressEvent{
+		OperationStatus: handler.Success,
+		Message:         constants.Complete,
+		ResourceModels:  response,
+	}
+}
---
+++ cfn-resources/stream-processor/cmd/resource/mappings_test.go
@@ -0,0 +1,341 @@
+// Copyright 2026 MongoDB Inc
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package resource_test
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/mongodb/mongodbatlas-cloudformation-resources/stream-processor/cmd/resource"
+	"github.com/mongodb/mongodbatlas-cloudformation-resources/util"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"go.mongodb.org/atlas-sdk/v20250312012/admin"
+)
+
+func assertJSONEqual(t *testing.T, expected, actual string) {
+	t.Helper()
+	var expectedJSON, actualJSON any
+	require.NoError(t, json.Unmarshal([]byte(expected), &expectedJSON))
+	require.NoError(t, json.Unmarshal([]byte(actual), &actualJSON))
+	assert.Equal(t, expectedJSON, actualJSON)
+}
+
+func TestConvertPipelineToSdk(t *testing.T) {
+	testCases := map[string]struct {
+		pipeline      string
+		expectedError bool
+	}{
+		"validPipeline": {
+			pipeline: `[{"$match": {"status": "active"}}]`,
+		},
+		"invalidJSON": {
+			pipeline:      `[{"$match": {"status": "active"}`,
+			expectedError: true,
+		},
+	}
+
+	for name, tc := range testCases {
+		t.Run(name, func(t *testing.T) {
+			result, err := resource.ConvertPipelineToSdk(tc.pipeline)
+			if tc.expectedError {
+				require.Error(t, err)
+				assert.Nil(t, result)
+			} else {
+				require.NoError(t, err)
+				assert.NotNil(t, result)
+			}
+		})
+	}
+}
+
+func TestConvertPipelineToString(t *testing.T) {
+	testCases := map[string]struct {
+		expectedJSON string
+		pipeline     []any
+	}{
+		"validPipeline": {
+			pipeline:     []any{map[string]any{"$match": map[string]any{"status": "active"}}},
+			expectedJSON: `[{"$match":{"status":"active"}}]`,
+		},
+		"nilPipeline": {
+			pipeline:     nil,
+			expectedJSON: `null`,
+		},
+	}
+
+	for name, tc := range testCases {
+		t.Run(name, func(t *testing.T) {
+			result, err := resource.ConvertPipelineToString(tc.pipeline)
+			require.NoError(t, err)
+			assertJSONEqual(t, tc.expectedJSON, result)
+		})
+	}
+}
+
+func TestConvertStatsToString(t *testing.T) {
+	result, err := resource.ConvertStatsToString(map[string]any{"bytesProcessed": 1000})
+	require.NoError(t, err)
+	assertJSONEqual(t, `{"bytesProcessed":1000}`, result)
+
+	result, err = resource.ConvertStatsToString(nil)
+	require.NoError(t, err)
+	assert.Empty(t, result)
+}
+
+func TestNewStreamProcessorReq(t *testing.T) {
+	validPipeline := `[{"$match": {"status": "active"}}]`
+	validDLQ := &resource.StreamsOptions{
+		Dlq: &resource.StreamsDLQ{
+			Coll:           util.StringPtr("dlq-collection"),
+			ConnectionName: util.StringPtr("dlq-connection"),
+			Db:             util.StringPtr("dlq-db"),
+		},
+	}
+
+	testCases := map[string]struct {
+		model         *resource.Model
+		expectedError bool
+		checkOptions  bool
+	}{
+		"minimalRequest": {
+			model: &resource.Model{
+				ProcessorName: util.StringPtr("test-processor"),
+				Pipeline:      util.StringPtr(validPipeline),
+			},
+		},
+		"withOptions": {
+			model: &resource.Model{
+				ProcessorName: util.StringPtr("test-processor"),
+				Pipeline:      util.StringPtr(validPipeline),
+				Options:       validDLQ,
+			},
+			checkOptions: true,
+		},
+		"invalidPipeline": {
+			model: &resource.Model{
+				ProcessorName: util.StringPtr("test-processor"),
+				Pipeline:      util.StringPtr(`invalid json`),
+			},
+			expectedError: true,
+		},
+	}
+
+	for name, tc := range testCases {
+		t.Run(name, func(t *testing.T) {
+			result, err := resource.NewStreamProcessorReq(tc.model)
+			if tc.expectedError {
+				require.Error(t, err)
+				assert.Nil(t, result)
+			} else {
+				require.NoError(t, err)
+				require.NotNil(t, result)
+				assert.Equal(t, "test-processor", result.GetName())
+				if tc.checkOptions {
+					require.NotNil(t, result.Options.Dlq)
+					assert.Equal(t, "dlq-collection", util.SafeString(result.Options.Dlq.Coll))
+				}
+			}
+		})
+	}
+}
+
+func TestNewStreamProcessorUpdateReq(t *testing.T) {
+	validPipeline := `[{"$match": {"status": "active"}}]`
+	validDLQ := &resource.StreamsOptions{
+		Dlq: &resource.StreamsDLQ{
+			Coll:           util.StringPtr("dlq-collection"),
+			ConnectionName: util.StringPtr("dlq-connection"),
+			Db:             util.StringPtr("dlq-db"),
+		},
+	}
+
+	testCases := map[string]struct {
+		model         *resource.Model
+		checkTenant   string
+		expectedError bool
+		checkOptions  bool
+	}{
+		"minimalRequest": {
+			model: &resource.Model{
+				ProjectId:     util.StringPtr("507f1f77bcf86cd799439011"),
+				ProcessorName: util.StringPtr("test-processor"),
+				WorkspaceName: util.StringPtr("workspace-1"),
+				Pipeline:      util.StringPtr(validPipeline),
+			},
+			checkTenant: "workspace-1",
+		},
+		"withOptions": {
+			model: &resource.Model{
+				ProjectId:     util.StringPtr("507f1f77bcf86cd799439011"),
+				ProcessorName: util.StringPtr("test-processor"),
+				WorkspaceName: util.StringPtr("workspace-1"),
+				Pipeline:      util.StringPtr(validPipeline),
+				Options:       validDLQ,
+			},
+			checkTenant:  "workspace-1",
+			checkOptions: true,
+		},
+		"invalidPipeline": {
+			model: &resource.Model{
+				ProjectId:     util.StringPtr("507f1f77bcf86cd799439011"),
+				ProcessorName: util.StringPtr("test-processor"),
+				WorkspaceName: util.StringPtr("workspace-1"),
+				Pipeline:      util.StringPtr(`invalid json`),
+			},
+			expectedError: true,
+		},
+	}
+
+	for name, tc := range testCases {
+		t.Run(name, func(t *testing.T) {
+			result, err := resource.NewStreamProcessorUpdateReq(tc.model)
+			if tc.expectedError {
+				require.Error(t, err)
+				assert.Nil(t, result)
+			} else {
+				require.NoError(t, err)
+				require.NotNil(t, result)
+				assert.Equal(t, "507f1f77bcf86cd799439011", result.GroupId)
+				assert.Equal(t, "test-processor", result.ProcessorName)
+				if tc.checkTenant != "" {
+					assert.Equal(t, tc.checkTenant, result.TenantName)
+				}
+				if tc.checkOptions {
+					require.NotNil(t, result.StreamsModifyStreamProcessor.Options.Dlq)
+					assert.Equal(t, "dlq-collection", result.StreamsModifyStreamProcessor.Options.Dlq.GetColl())
+				}
+			}
+		})
+	}
+}
+
+func TestGetStreamProcessorModel(t *testing.T) {
+	validDLQ := &admin.StreamsDLQ{
+		Coll:           admin.PtrString("dlq-collection"),
+		ConnectionName: admin.PtrString("dlq-connection"),
+		Db:             admin.PtrString("dlq-db"),
+	}
+	currentModelWithDLQ := &resource.Model{
+		Options: &resource.StreamsOptions{
+			Dlq: &resource.StreamsDLQ{
+				Coll:           util.StringPtr("existing-dlq-collection"),
+				ConnectionName: util.StringPtr("existing-dlq-connection"),
+				Db:             util.StringPtr("existing-dlq-db"),
+			},
+		},
+	}
+
+	testCases := map[string]struct {
+		streamProcessor *admin.StreamsProcessorWithStats
+		currentModel    *resource.Model
+		checkFields     []string
+	}{
+		"minimalConversion": {
+			streamProcessor: &admin.StreamsProcessorWithStats{
+				Name:  "test-processor",
+				Id:    "507f1f77bcf86cd799439011",
+				State: "CREATED",
+			},
+			checkFields: []string{"name", "id", "state"},
+		},
+		"withAllFields": {
+			streamProcessor: &admin.StreamsProcessorWithStats{
+				Name:     "test-processor",
+				Id:       "507f1f77bcf86cd799439011",
+				State:    "STARTED",
+				Pipeline: []any{map[string]any{"$match": map[string]any{"status": "active"}}},
+				Stats:    map[string]any{"bytesProcessed": 5000, "recordsProcessed": 500},
+				Options:  &admin.StreamsOptions{Dlq: validDLQ},
+			},
+			currentModel: &resource.Model{
+				ProjectId:     util.StringPtr("507f1f77bcf86cd799439011"),
+				ProcessorName: util.StringPtr("test-processor"),
+			},
+			checkFields: []string{"all"},
+		},
+		"preserveCurrentModelOptions": {
+			streamProcessor: &admin.StreamsProcessorWithStats{
+				Name:  "test-processor",
+				Id:    "507f1f77bcf86cd799439011",
+				State: "CREATED",
+			},
+			currentModel: currentModelWithDLQ,
+			checkFields:  []string{"preservedOptions"},
+		},
+	}
+
+	for name, tc := range testCases {
+		t.Run(name, func(t *testing.T) {
+			result, err := resource.GetStreamProcessorModel(tc.streamProcessor, tc.currentModel)
+			require.NoError(t, err)
+			require.NotNil(t, result)
+
+			for _, field := range tc.checkFields {
+				switch field {
+				case "name":
+					assert.Equal(t, "test-processor", util.SafeString(result.ProcessorName))
+				case "id":
+					assert.Equal(t, "507f1f77bcf86cd799439011", util.SafeString(result.Id))
+				case "state":
+					assert.Equal(t, tc.streamProcessor.State, util.SafeString(result.State))
+				case "all":
+					assert.Equal(t, "test-processor", util.SafeString(result.ProcessorName))
+					assert.NotNil(t, result.Pipeline)
+					assert.NotNil(t, result.Stats)
+					require.NotNil(t, result.Options.Dlq)
+					assert.Equal(t, "dlq-collection", util.SafeString(result.Options.Dlq.Coll))
+				case "preservedOptions":
+					require.NotNil(t, result.Options.Dlq)
+					assert.Equal(t, "existing-dlq-collection", util.SafeString(result.Options.Dlq.Coll))
+				}
+			}
+		})
+	}
+}
+
+func TestRoundTripConversions(t *testing.T) {
+	t.Run("pipelineRoundTrip", func(t *testing.T) {
+		originalPipeline := `[{"$match": {"status": "active"}}, {"$group": {"_id": "$category", "count": {"$sum": 1}}}]`
+		sdkPipeline, err := resource.ConvertPipelineToSdk(originalPipeline)
+		require.NoError(t, err)
+		convertedBack, err := resource.ConvertPipelineToString(sdkPipeline)
+		require.NoError(t, err)
+
+		var original, converted any
+		require.NoError(t, json.Unmarshal([]byte(originalPipeline), &original))
+		require.NoError(t, json.Unmarshal([]byte(convertedBack), &converted))
+		assert.Equal(t, original, converted)
+	})
+
+	t.Run("statsRoundTrip", func(t *testing.T) {
+		originalStats := map[string]any{
+			"bytesProcessed":   1000,
+			"recordsProcessed": 100,
+			"nested":           map[string]any{"value": 42},
+		}
+		statsString, err := resource.ConvertStatsToString(originalStats)
+		require.NoError(t, err)
+
+		var parsedStats any
+		require.NoError(t, json.Unmarshal([]byte(statsString), &parsedStats))
+		parsedMap := parsedStats.(map[string]any)
+
+		assert.InDelta(t, float64(1000), parsedMap["bytesProcessed"], 0.01)
+		assert.InDelta(t, float64(100), parsedMap["recordsProcessed"], 0.01)
+		nested := parsedMap["nested"].(map[string]any)
+		assert.InDelta(t, float64(42), nested["value"], 0.01)
+	})
+}

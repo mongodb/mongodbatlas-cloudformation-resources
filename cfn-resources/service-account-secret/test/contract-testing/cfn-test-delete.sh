cded87adabfa574a8f7acacaa62e445771a1dcf6
Rakhul S Prakash <rakhul.s.prakash@peerislands.io>
2026-02-13 15:19:08 +0530

    add service-account-secret

---
+++ cfn-resources/service-account-secret/test/contract-testing/cfn-test-delete.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# This tool deletes the mongodb resources used for `cfn test` as inputs.
+set -o errexit
+set -o nounset
+set -o pipefail
+
+if [ ! -f ./inputs/inputs_1_create.json ]; then
+	echo "No inputs file found, nothing to clean up"
+	exit 0
+fi
+
+orgId=$(jq -r '.OrgId' ./inputs/inputs_1_create.json)
+clientId=$(jq -r '.ClientId' ./inputs/inputs_1_create.json)
+
+# Delete service account using Atlas CLI (this will also delete all its secrets)
+if atlas api serviceAccounts deleteOrgServiceAccount --orgId "$orgId" --clientId "$clientId" --version "2024-08-05" --output json 2>/dev/null; then
+	echo "Service account $clientId deletion OK"
+else
+	exitCode=$?
+	if [ $exitCode -eq 0 ]; then
+		echo "Service account $clientId deletion OK"
+	else
+		echo "Warning: Failed cleaning service account: $clientId (may already be deleted)"
+	fi
+fi

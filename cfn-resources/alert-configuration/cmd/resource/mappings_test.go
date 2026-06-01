13f99d51e060178b34281384a7c7a5e8d53884b5
Leo Antoli <430982+lantoli@users.noreply.github.com>
2026-06-01 12:36:48 +0200

    chore: Remove license header and function comment from mappings_test.go

--- cfn-resources/alert-configuration/cmd/resource/mappings_test.go
+++ cfn-resources/alert-configuration/cmd/resource/mappings_test.go
@@ -1,17 +1,3 @@
-// Copyright 2026 MongoDB Inc
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 package resource_test
 
 import (
@@ -22,10 +8,6 @@ import (
 	"go.mongodb.org/atlas-sdk/v20250312013/admin"
 )
 
-// TestConvertToMongoModel_ClearsAtlasReturnedThreshold verifies that when Atlas returns
-// both MetricThreshold and Threshold (backward compatibility, see HELP-91242),
-// ConvertToMongoModel clears whichever field is absent from the CFN model,
-// preventing DUPLICATE_THRESHOLD_FIELD on the subsequent PUT.
 func TestConvertToMongoModel_ClearsAtlasReturnedThreshold(t *testing.T) {
 	metricThresholdName := "NORMALIZED_SYSTEM_CPU_USER"
 
